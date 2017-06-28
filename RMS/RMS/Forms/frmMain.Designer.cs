115f7265aaf4f21298c3a660de1e60f06ea39b67
woxzonedotnet <woxzonedotnet@gmail.com>
2017-06-28 14:58:21 +0530

    Main Structure

---
+++ RMS/RMS/Forms/frmMain.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace RMS.Forms
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.masterDataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supplierCategoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.supplierDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.departmentDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoryDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.subCategoryDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locationDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itemDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addStewardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuDepartmentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCategoryDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.recipeDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableCategoryDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bankCardsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setupSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.transactionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.processToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.masterDataToolStripMenuItem,
+            this.transactionToolStripMenuItem,
+            this.processToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.userToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(843, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // masterDataToolStripMenuItem
+            // 
+            this.masterDataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.supplierCategoryToolStripMenuItem,
+            this.supplierDetailsToolStripMenuItem,
+            this.departmentDetailsToolStripMenuItem,
+            this.categoryDetailsToolStripMenuItem,
+            this.subCategoryDetailsToolStripMenuItem,
+            this.locationDetailsToolStripMenuItem,
+            this.itemDetailsToolStripMenuItem,
+            this.addStewardToolStripMenuItem,
+            this.customerDetailsToolStripMenuItem,
+            this.menuDepartmentToolStripMenuItem,
+            this.menuCategoryDetailsToolStripMenuItem,
+            this.recipeDetailsToolStripMenuItem,
+            this.menuDetailsToolStripMenuItem,
+            this.tableCategoryDetailsToolStripMenuItem,
+            this.tableDetailsToolStripMenuItem,
+            this.bankCardsToolStripMenuItem,
+            this.setupSettingsToolStripMenuItem});
+            this.masterDataToolStripMenuItem.Name = "masterDataToolStripMenuItem";
+            this.masterDataToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.masterDataToolStripMenuItem.Text = "Master Data";
+            // 
+            // supplierCategoryToolStripMenuItem
+            // 
+            this.supplierCategoryToolStripMenuItem.Name = "supplierCategoryToolStripMenuItem";
+            this.supplierCategoryToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.supplierCategoryToolStripMenuItem.Text = "Supplier Category";
+            // 
+            // supplierDetailsToolStripMenuItem
+            // 
+            this.supplierDetailsToolStripMenuItem.Name = "supplierDetailsToolStripMenuItem";
+            this.supplierDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.supplierDetailsToolStripMenuItem.Text = "Supplier Details";
+            // 
+            // departmentDetailsToolStripMenuItem
+            // 
+            this.departmentDetailsToolStripMenuItem.Name = "departmentDetailsToolStripMenuItem";
+            this.departmentDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.departmentDetailsToolStripMenuItem.Text = "Department Details";
+            // 
+            // categoryDetailsToolStripMenuItem
+            // 
+            this.categoryDetailsToolStripMenuItem.Name = "categoryDetailsToolStripMenuItem";
+            this.categoryDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.categoryDetailsToolStripMenuItem.Text = "Category Details";
+            // 
+            // subCategoryDetailsToolStripMenuItem
+            // 
+            this.subCategoryDetailsToolStripMenuItem.Name = "subCategoryDetailsToolStripMenuItem";
+            this.subCategoryDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.subCategoryDetailsToolStripMenuItem.Text = "Sub Category Details";
+            // 
+            // locationDetailsToolStripMenuItem
+            // 
+            this.locationDetailsToolStripMenuItem.Name = "locationDetailsToolStripMenuItem";
+            this.locationDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.locationDetailsToolStripMenuItem.Text = "Location Details";
+            // 
+            // itemDetailsToolStripMenuItem
+            // 
+            this.itemDetailsToolStripMenuItem.Name = "itemDetailsToolStripMenuItem";
+            this.itemDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.itemDetailsToolStripMenuItem.Text = "Item Details";
+            // 
+            // addStewardToolStripMenuItem
+            // 
+            this.addStewardToolStripMenuItem.Name = "addStewardToolStripMenuItem";
+            this.addStewardToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.addStewardToolStripMenuItem.Text = "Add Steward";
+            // 
+            // customerDetailsToolStripMenuItem
+            // 
+            this.customerDetailsToolStripMenuItem.Name = "customerDetailsToolStripMenuItem";
+            this.customerDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.customerDetailsToolStripMenuItem.Text = "Customer Details";
+            // 
+            // menuDepartmentToolStripMenuItem
+            // 
+            this.menuDepartmentToolStripMenuItem.Name = "menuDepartmentToolStripMenuItem";
+            this.menuDepartmentToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.menuDepartmentToolStripMenuItem.Text = "Menu Department";
+            // 
+            // menuCategoryDetailsToolStripMenuItem
+            // 
+            this.menuCategoryDetailsToolStripMenuItem.Name = "menuCategoryDetailsToolStripMenuItem";
+            this.menuCategoryDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.menuCategoryDetailsToolStripMenuItem.Text = "Menu Category Details";
+            // 
+            // recipeDetailsToolStripMenuItem
+            // 
+            this.recipeDetailsToolStripMenuItem.Name = "recipeDetailsToolStripMenuItem";
+            this.recipeDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.recipeDetailsToolStripMenuItem.Text = "Recipe Details";
+            // 
+            // menuDetailsToolStripMenuItem
+            // 
+            this.menuDetailsToolStripMenuItem.Name = "menuDetailsToolStripMenuItem";
+            this.menuDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.menuDetailsToolStripMenuItem.Text = "Menu Details";
+            // 
+            // tableCategoryDetailsToolStripMenuItem
+            // 
+            this.tableCategoryDetailsToolStripMenuItem.Name = "tableCategoryDetailsToolStripMenuItem";
+            this.tableCategoryDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.tableCategoryDetailsToolStripMenuItem.Text = "Table Category Details";
+            // 
+            // tableDetailsToolStripMenuItem
+            // 
+            this.tableDetailsToolStripMenuItem.Name = "tableDetailsToolStripMenuItem";
+            this.tableDetailsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.tableDetailsToolStripMenuItem.Text = "Table Details";
+            // 
+            // bankCardsToolStripMenuItem
+            // 
+            this.bankCardsToolStripMenuItem.Name = "bankCardsToolStripMenuItem";
+            this.bankCardsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.bankCardsToolStripMenuItem.Text = "Bank Cards";
+            // 
+            // setupSettingsToolStripMenuItem
+            // 
+            this.setupSettingsToolStripMenuItem.Name = "setupSettingsToolStripMenuItem";
+            this.setupSettingsToolStripMenuItem.Size = new System.Drawing.Size(194, 22);
+            this.setupSettingsToolStripMenuItem.Text = "Setup Settings";
+            // 
+            // transactionToolStripMenuItem
+            // 
+            this.transactionToolStripMenuItem.Name = "transactionToolStripMenuItem";
+            this.transactionToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.transactionToolStripMenuItem.Text = "Transaction";
+            // 
+            // processToolStripMenuItem
+            // 
+            this.processToolStripMenuItem.Name = "processToolStripMenuItem";
+            this.processToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.processToolStripMenuItem.Text = "Process";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(42, 20);
+            this.userToolStripMenuItem.Text = "User";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 261);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "frmMain";
+            this.Text = "Restaurants Management System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem masterDataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supplierCategoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem supplierDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem departmentDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoryDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem subCategoryDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locationDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addStewardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuDepartmentToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuCategoryDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem recipeDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem menuDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tableCategoryDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tableDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bankCardsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setupSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem transactionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem processToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+    }
+}
---
+++ RMS/RMS/StringList.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMS
+{
+    class StringList
+    {
+        ArrayList m_alMain;
+
+        public StringList()
+        {
+            m_alMain = new ArrayList();
+        }
+
+        ~StringList()
+        {
+            m_alMain.Clear();
+        }
+        public int Add(string s)
+        {
+            m_alMain.Add(s);
+            return m_alMain.Count;
+        }
+        public StringList AddRange(StringList sl)
+        {
+            m_alMain.AddRange(sl.m_alMain);
+            return null;
+        }
+
+        public int AddNoDuplicate(string s)
+        {
+            if (m_alMain.IndexOf(s) == -1)
+                m_alMain.Add(s);
+            return m_alMain.Count;
+        }
+
+        public int Insert(int index, string s)
+        {
+            m_alMain.Insert(index, s);
+            return m_alMain.Count;
+        }
+
+        public int Remove(String s)
+        {
+            m_alMain.Remove(s);
+            return m_alMain.Count;
+        }
+        public int Replace(string sFind, string sReplace)
+        {
+            int index = m_alMain.IndexOf(sFind);
+            if (index > -1)
+            {
+                m_alMain.RemoveAt(index);
+                m_alMain.Insert(index, sReplace);
+            }
+            return m_alMain.Count;
+        }
+
+        public void Clear()
+        {
+            m_alMain.Clear();
+        }
+
+        public String this[int index]
+        {
+            get
+            {
+                return (string)m_alMain[index];
+            }
+            set
+            {
+                if (index >= m_alMain.Count)
+                    m_alMain.Add(value);
+                else
+                    m_alMain[index] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            string sRHS = "";
+            int index = 0;
+            while (index < m_alMain.Count)
+            {
+                sRHS += (string)m_alMain[index++] + "\n";
+            }
+            return sRHS;
+        }
+
+        public String ToString(string sSeperator)
+        {
+            string sRHS = "";
+            int index = 0;
+            while (index < m_alMain.Count)
+            {
+                sRHS += (string)m_alMain[index++];
+                if (index < m_alMain.Count)
+                    sRHS += sSeperator;
+            }
+            return sRHS;
+        }
+
+        public void Sort()
+        {
+            m_alMain.Sort();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return m_alMain.Count;
+            }
+        }
+
+        public int IndexOf(string sFind)
+        {
+            return m_alMain.IndexOf(sFind);
+        }
+
+        public static implicit operator string[](StringList sl)
+        {
+            string[] sLHS = new string[sl.m_alMain.Count];
+            int index = 0;
+            while (index < sl.m_alMain.Count)
+            {
+                sLHS[index] = (string)sl.m_alMain[index];
+                index++;
+            }
+            return sLHS;
+        }
+
+        public static implicit operator StringList(string[] sa)
+        {
+            StringList sl = new StringList();
+            for (int index = 0; index < sa.Length; index++)
+            {
+                sl.Add(sa[index]);
+            }
+            return sl;
+        }
+
+        public static implicit operator StringList(object[] sa)
+        {
+            StringList sl = new StringList();
+            for (int index = 0; index < sa.Length; index++)
+            {
+                sl.Add(sa[index].ToString());
+            }
+            return sl;
+        }
+    }
+}
