429395c3ea3ffc0be7041905c8da8dbb7790cdb2
woxzonedotnet <woxzonedotnet@gmail.com>
2017-06-28 15:11:09 +0530

    aa

--- RMS/RMS/Forms/frmMain.Designer.cs
+++ RMS/RMS/Forms/frmMain.Designer.cs
@@ -230,7 +230,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(843, 261);
+            this.ClientSize = new System.Drawing.Size(843, 289);
             this.Controls.Add(this.menuStrip1);
             this.Name = "frmMain";
             this.Text = "Restaurants Management System";
