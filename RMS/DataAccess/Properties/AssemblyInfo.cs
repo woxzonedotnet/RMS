f6a8e1000008ff8bd810c13df8094c04d60c680e
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-06-28 15:22:35 +0530

    Main Structure

--- RMS/DataAccess/Properties/AssemblyInfo.cs
+++ RMS/DataAccess/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BusinessLogic")]
+[assembly: AssemblyTitle("DataAccess")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BusinessLogic")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("DataAccess")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0b5d3ec0-6294-4a11-b2ee-59aaa3038d7f")]
+[assembly: Guid("6ac9111a-d3f5-4ab4-835a-3efc36fc407a")]
 
 // Version information for an assembly consists of the following four values:
 //
