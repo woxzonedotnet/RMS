84043169edad2634aeb8c69fced29f8749e825a6
dilshan8kt <dilshan.kt@gmail.com>
2017-06-29 11:04:01 +0530

    SupplierCategory

--- RMS/RMS/Forms/frmSupplierCategory.Designer.cs
+++ RMS/RMS/Forms/frmSupplierCategory.Designer.cs
@@ -28,17 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSupplierCategory));
             this.lblSupplierCode = new System.Windows.Forms.Label();
             this.lblDescription = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.txtSupplierCode = new System.Windows.Forms.TextBox();
             this.txtDescription = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnSearch = new System.Windows.Forms.Button();
-            this.columnComboBox1 = new RMS.ColumnComboBox();
             this.SuspendLayout();
             // 
             // lblSupplierCode
@@ -61,16 +58,6 @@
             this.lblDescription.TabIndex = 1;
             this.lblDescription.Text = "Description";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 104);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(45, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
-            // 
             // txtSupplierCode
             // 
             this.txtSupplierCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -90,7 +77,7 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(47, 138);
+            this.btnSave.Location = new System.Drawing.Point(74, 101);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 36);
             this.btnSave.TabIndex = 6;
@@ -100,7 +87,7 @@
             // btnClear
             // 
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(128, 138);
+            this.btnClear.Location = new System.Drawing.Point(155, 101);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 36);
             this.btnClear.TabIndex = 7;
@@ -110,7 +97,7 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(209, 138);
+            this.btnClose.Location = new System.Drawing.Point(236, 101);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 36);
             this.btnClose.TabIndex = 8;
@@ -127,31 +114,17 @@
             this.btnSearch.TabIndex = 9;
             this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // columnComboBox1
-            // 
-            this.columnComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.columnComboBox1.DropDownWidth = 17;
-            this.columnComboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.columnComboBox1.FormattingEnabled = true;
-            this.columnComboBox1.Location = new System.Drawing.Point(120, 101);
-            this.columnComboBox1.Name = "columnComboBox1";
-            this.columnComboBox1.Size = new System.Drawing.Size(150, 22);
-            this.columnComboBox1.TabIndex = 5;
-            this.columnComboBox1.ViewColumn = 0;
-            // 
             // frmSupplierCategory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(330, 194);
+            this.ClientSize = new System.Drawing.Size(330, 154);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.columnComboBox1);
             this.Controls.Add(this.txtDescription);
             this.Controls.Add(this.txtSupplierCode);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.lblDescription);
             this.Controls.Add(this.lblSupplierCode);
             this.Name = "frmSupplierCategory";
@@ -166,10 +139,8 @@
 
         private System.Windows.Forms.Label lblSupplierCode;
         private System.Windows.Forms.Label lblDescription;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtSupplierCode;
         private System.Windows.Forms.TextBox txtDescription;
-        private ColumnComboBox columnComboBox1;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnClose;
