1355080f3f33d5213b6112b8f5cfa07b8e21bcb8
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-01 13:21:36 +0530

    DepartmentDetailsDesign

---
+++ RMS/RMS/Forms/frmDepartmentDetaild.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace RMS.Forms
+{
+    partial class frmDepartmentDetaild
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtDepartmentName = new System.Windows.Forms.TextBox();
+            this.txtDepartmentCode = new System.Windows.Forms.TextBox();
+            this.lblDepartmentName = new System.Windows.Forms.Label();
+            this.lblDepartmentCode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(262, 85);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 36);
+            this.btnClose.TabIndex = 45;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(181, 85);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 44;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(100, 85);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 36);
+            this.btnEdit.TabIndex = 43;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(19, 85);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 42;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtDepartmentName
+            // 
+            this.txtDepartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartmentName.Location = new System.Drawing.Point(139, 49);
+            this.txtDepartmentName.Name = "txtDepartmentName";
+            this.txtDepartmentName.Size = new System.Drawing.Size(198, 21);
+            this.txtDepartmentName.TabIndex = 41;
+            // 
+            // txtDepartmentCode
+            // 
+            this.txtDepartmentCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartmentCode.Location = new System.Drawing.Point(139, 13);
+            this.txtDepartmentCode.Name = "txtDepartmentCode";
+            this.txtDepartmentCode.Size = new System.Drawing.Size(198, 21);
+            this.txtDepartmentCode.TabIndex = 40;
+            // 
+            // lblDepartmentName
+            // 
+            this.lblDepartmentName.AutoSize = true;
+            this.lblDepartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartmentName.Location = new System.Drawing.Point(15, 52);
+            this.lblDepartmentName.Name = "lblDepartmentName";
+            this.lblDepartmentName.Size = new System.Drawing.Size(118, 16);
+            this.lblDepartmentName.TabIndex = 39;
+            this.lblDepartmentName.Text = "Department Name";
+            // 
+            // lblDepartmentCode
+            // 
+            this.lblDepartmentCode.AutoSize = true;
+            this.lblDepartmentCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartmentCode.Location = new System.Drawing.Point(15, 16);
+            this.lblDepartmentCode.Name = "lblDepartmentCode";
+            this.lblDepartmentCode.Size = new System.Drawing.Size(114, 16);
+            this.lblDepartmentCode.TabIndex = 38;
+            this.lblDepartmentCode.Text = "Department Code";
+            // 
+            // frmDepartmentDetaild
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 132);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtDepartmentName);
+            this.Controls.Add(this.txtDepartmentCode);
+            this.Controls.Add(this.lblDepartmentName);
+            this.Controls.Add(this.lblDepartmentCode);
+            this.Name = "frmDepartmentDetaild";
+            this.Text = "Department Details";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtDepartmentName;
+        private System.Windows.Forms.TextBox txtDepartmentCode;
+        private System.Windows.Forms.Label lblDepartmentName;
+        private System.Windows.Forms.Label lblDepartmentCode;
+    }
+}
