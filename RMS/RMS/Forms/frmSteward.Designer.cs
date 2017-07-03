f9a0477ae11915ff1f9a9a4c273837be9faf9abc
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-03 10:42:48 +0530

    StewardEdit

--- RMS/RMS/Forms/frmSteward.Designer.cs
+++ RMS/RMS/Forms/frmSteward.Designer.cs
@@ -28,22 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtStewardName = new System.Windows.Forms.TextBox();
+            this.txtStewardID = new System.Windows.Forms.TextBox();
+            this.lblStewardName = new System.Windows.Forms.Label();
+            this.lblStewardID = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.chkHold = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Image = global::RMS.Properties.Resources._1498666014_basics_19;
+            this.btnSearch.Location = new System.Drawing.Point(269, 7);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(35, 31);
+            this.btnSearch.TabIndex = 17;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(230, 141);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 36);
+            this.btnClose.TabIndex = 16;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(149, 141);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(68, 141);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // txtStewardName
+            // 
+            this.txtStewardName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStewardName.Location = new System.Drawing.Point(113, 48);
+            this.txtStewardName.Name = "txtStewardName";
+            this.txtStewardName.Size = new System.Drawing.Size(150, 21);
+            this.txtStewardName.TabIndex = 13;
+            // 
+            // txtStewardID
+            // 
+            this.txtStewardID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStewardID.Location = new System.Drawing.Point(113, 12);
+            this.txtStewardID.Name = "txtStewardID";
+            this.txtStewardID.Size = new System.Drawing.Size(150, 21);
+            this.txtStewardID.TabIndex = 12;
+            // 
+            // lblStewardName
+            // 
+            this.lblStewardName.AutoSize = true;
+            this.lblStewardName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStewardName.Location = new System.Drawing.Point(13, 51);
+            this.lblStewardName.Name = "lblStewardName";
+            this.lblStewardName.Size = new System.Drawing.Size(97, 16);
+            this.lblStewardName.TabIndex = 11;
+            this.lblStewardName.Text = "Steward Name";
+            // 
+            // lblStewardID
+            // 
+            this.lblStewardID.AutoSize = true;
+            this.lblStewardID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStewardID.Location = new System.Drawing.Point(13, 15);
+            this.lblStewardID.Name = "lblStewardID";
+            this.lblStewardID.Size = new System.Drawing.Size(73, 16);
+            this.lblStewardID.TabIndex = 10;
+            this.lblStewardID.Text = "Steward ID";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(113, 84);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 21);
+            this.txtPassword.TabIndex = 19;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(13, 87);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(68, 16);
+            this.lblPassword.TabIndex = 18;
+            this.lblPassword.Text = "Password";
+            // 
+            // chkHold
+            // 
+            this.chkHold.AutoSize = true;
+            this.chkHold.Location = new System.Drawing.Point(113, 114);
+            this.chkHold.Name = "chkHold";
+            this.chkHold.Size = new System.Drawing.Size(48, 17);
+            this.chkHold.TabIndex = 20;
+            this.chkHold.Text = "Hold";
+            this.chkHold.UseVisualStyleBackColor = true;
             // 
             // frmSteward
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(319, 189);
+            this.Controls.Add(this.chkHold);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtStewardName);
+            this.Controls.Add(this.txtStewardID);
+            this.Controls.Add(this.lblStewardName);
+            this.Controls.Add(this.lblStewardID);
             this.MaximizeBox = false;
             this.Name = "frmSteward";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Steward";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtStewardName;
+        private System.Windows.Forms.TextBox txtStewardID;
+        private System.Windows.Forms.Label lblStewardName;
+        private System.Windows.Forms.Label lblStewardID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.CheckBox chkHold;
     }
 }
