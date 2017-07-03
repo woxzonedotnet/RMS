5ffdde85542ccafbbda2e9417ff20c2b8abf58ad
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-03 10:07:06 +0530

    subCategoryMasterDetailsDesign

---
+++ RMS/RMS/Forms/frmSubCategoryMasterDetails.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace RMS.Forms
+{
+    partial class frmSubCategoryMasterDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCategoryCode = new System.Windows.Forms.TextBox();
+            this.txtDepartmentCode = new System.Windows.Forms.TextBox();
+            this.lblCategoryCode = new System.Windows.Forms.Label();
+            this.lblDepartmentCode = new System.Windows.Forms.Label();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.txtDepartmentName = new System.Windows.Forms.TextBox();
+            this.lblCategoryName = new System.Windows.Forms.Label();
+            this.lblDepartmentName = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCategoryCode
+            // 
+            this.txtCategoryCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoryCode.Location = new System.Drawing.Point(125, 48);
+            this.txtCategoryCode.Name = "txtCategoryCode";
+            this.txtCategoryCode.Size = new System.Drawing.Size(135, 21);
+            this.txtCategoryCode.TabIndex = 7;
+            // 
+            // txtDepartmentCode
+            // 
+            this.txtDepartmentCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartmentCode.Location = new System.Drawing.Point(125, 12);
+            this.txtDepartmentCode.Name = "txtDepartmentCode";
+            this.txtDepartmentCode.Size = new System.Drawing.Size(135, 21);
+            this.txtDepartmentCode.TabIndex = 6;
+            // 
+            // lblCategoryCode
+            // 
+            this.lblCategoryCode.AutoSize = true;
+            this.lblCategoryCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoryCode.Location = new System.Drawing.Point(9, 51);
+            this.lblCategoryCode.Name = "lblCategoryCode";
+            this.lblCategoryCode.Size = new System.Drawing.Size(99, 16);
+            this.lblCategoryCode.TabIndex = 5;
+            this.lblCategoryCode.Text = "Category Code";
+            // 
+            // lblDepartmentCode
+            // 
+            this.lblDepartmentCode.AutoSize = true;
+            this.lblDepartmentCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartmentCode.Location = new System.Drawing.Point(9, 15);
+            this.lblDepartmentCode.Name = "lblDepartmentCode";
+            this.lblDepartmentCode.Size = new System.Drawing.Size(114, 16);
+            this.lblDepartmentCode.TabIndex = 4;
+            this.lblDepartmentCode.Text = "Department Code";
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCategoryName.Location = new System.Drawing.Point(404, 48);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(198, 21);
+            this.txtCategoryName.TabIndex = 11;
+            // 
+            // txtDepartmentName
+            // 
+            this.txtDepartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDepartmentName.Location = new System.Drawing.Point(404, 12);
+            this.txtDepartmentName.Name = "txtDepartmentName";
+            this.txtDepartmentName.Size = new System.Drawing.Size(198, 21);
+            this.txtDepartmentName.TabIndex = 10;
+            // 
+            // lblCategoryName
+            // 
+            this.lblCategoryName.AutoSize = true;
+            this.lblCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoryName.Location = new System.Drawing.Point(280, 51);
+            this.lblCategoryName.Name = "lblCategoryName";
+            this.lblCategoryName.Size = new System.Drawing.Size(103, 16);
+            this.lblCategoryName.TabIndex = 9;
+            this.lblCategoryName.Text = "Category Name";
+            // 
+            // lblDepartmentName
+            // 
+            this.lblDepartmentName.AutoSize = true;
+            this.lblDepartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartmentName.Location = new System.Drawing.Point(280, 15);
+            this.lblDepartmentName.Name = "lblDepartmentName";
+            this.lblDepartmentName.Size = new System.Drawing.Size(118, 16);
+            this.lblDepartmentName.TabIndex = 8;
+            this.lblDepartmentName.Text = "Department Name";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(528, 253);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 36);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(447, 253);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(366, 253);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 88);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(590, 150);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // frmSubCategoryMasterDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 298);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtCategoryName);
+            this.Controls.Add(this.txtDepartmentName);
+            this.Controls.Add(this.lblCategoryName);
+            this.Controls.Add(this.lblDepartmentName);
+            this.Controls.Add(this.txtCategoryCode);
+            this.Controls.Add(this.txtDepartmentCode);
+            this.Controls.Add(this.lblCategoryCode);
+            this.Controls.Add(this.lblDepartmentCode);
+            this.Name = "frmSubCategoryMasterDetails";
+            this.Text = "Sub Category Master Details";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCategoryCode;
+        private System.Windows.Forms.TextBox txtDepartmentCode;
+        private System.Windows.Forms.Label lblCategoryCode;
+        private System.Windows.Forms.Label lblDepartmentCode;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.TextBox txtDepartmentName;
+        private System.Windows.Forms.Label lblCategoryName;
+        private System.Windows.Forms.Label lblDepartmentName;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
