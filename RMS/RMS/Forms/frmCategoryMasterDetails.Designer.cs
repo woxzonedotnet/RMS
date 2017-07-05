e378c9807bf67f62909346c810a10fd685c8fdcd
dilshan8kt <dilshan.kt@gmail.com>
2017-07-05 17:21:17 +0530

    Edit Category Form

--- RMS/RMS/Forms/frmCategoryMasterDetails.Designer.cs
+++ RMS/RMS/Forms/frmCategoryMasterDetails.Designer.cs
@@ -29,35 +29,35 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCategoryMasterDetails));
-            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtCategoryName = new System.Windows.Forms.TextBox();
-            this.cmbDepartmentName = new RMS.ColumnComboBox();
             this.txtCategoryCode = new System.Windows.Forms.TextBox();
             this.lblDepartmentName = new System.Windows.Forms.Label();
-            this.btnRemove = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbStatus = new RMS.ColumnComboBox();
+            this.cmbDepartmentName = new RMS.ColumnComboBox();
             this.SuspendLayout();
             // 
-            // btnAdd
+            // btnUpdate
             // 
-            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(165, 112);
-            this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 25);
-            this.btnAdd.TabIndex = 39;
-            this.btnAdd.Text = "Add";
-            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(97, 157);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 36);
+            this.btnUpdate.TabIndex = 39;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(259, 291);
+            this.btnClose.Location = new System.Drawing.Point(260, 157);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 36);
             this.btnClose.TabIndex = 38;
@@ -67,7 +67,7 @@
             // btnClear
             // 
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(178, 291);
+            this.btnClear.Location = new System.Drawing.Point(179, 157);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 36);
             this.btnClear.TabIndex = 37;
@@ -77,26 +77,19 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(97, 291);
+            this.btnSave.Location = new System.Drawing.Point(16, 157);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 36);
             this.btnSave.TabIndex = 36;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(14, 147);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(320, 129);
-            this.dataGridView1.TabIndex = 35;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 86);
+            this.label2.Location = new System.Drawing.Point(14, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 16);
             this.label2.TabIndex = 34;
@@ -106,7 +99,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 50);
+            this.label1.Location = new System.Drawing.Point(14, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 16);
             this.label1.TabIndex = 33;
@@ -115,61 +108,83 @@
             // txtCategoryName
             // 
             this.txtCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategoryName.Location = new System.Drawing.Point(136, 83);
+            this.txtCategoryName.Location = new System.Drawing.Point(138, 77);
             this.txtCategoryName.Name = "txtCategoryName";
             this.txtCategoryName.Size = new System.Drawing.Size(198, 21);
             this.txtCategoryName.TabIndex = 32;
             // 
-            // cmbDepartmentName
-            // 
-            this.cmbDepartmentName.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.cmbDepartmentName.DropDownWidth = 17;
-            this.cmbDepartmentName.FormattingEnabled = true;
-            this.cmbDepartmentName.Location = new System.Drawing.Point(136, 11);
-            this.cmbDepartmentName.Name = "cmbDepartmentName";
-            this.cmbDepartmentName.Size = new System.Drawing.Size(198, 21);
-            this.cmbDepartmentName.TabIndex = 31;
-            this.cmbDepartmentName.ViewColumn = 0;
-            // 
             // txtCategoryCode
             // 
             this.txtCategoryCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategoryCode.Location = new System.Drawing.Point(136, 47);
+            this.txtCategoryCode.Location = new System.Drawing.Point(138, 44);
             this.txtCategoryCode.Name = "txtCategoryCode";
-            this.txtCategoryCode.Size = new System.Drawing.Size(198, 21);
+            this.txtCategoryCode.Size = new System.Drawing.Size(157, 21);
             this.txtCategoryCode.TabIndex = 30;
             // 
             // lblDepartmentName
             // 
             this.lblDepartmentName.AutoSize = true;
             this.lblDepartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDepartmentName.Location = new System.Drawing.Point(12, 12);
+            this.lblDepartmentName.Location = new System.Drawing.Point(14, 12);
             this.lblDepartmentName.Name = "lblDepartmentName";
             this.lblDepartmentName.Size = new System.Drawing.Size(118, 16);
             this.lblDepartmentName.TabIndex = 29;
             this.lblDepartmentName.Text = "Department Name";
             // 
-            // btnRemove
+            // btnSearch
             // 
-            this.btnRemove.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemove.Location = new System.Drawing.Point(259, 112);
-            this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(75, 25);
-            this.btnRemove.TabIndex = 40;
-            this.btnRemove.Text = "Remove";
-            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Image = global::RMS.Properties.Resources._1498666014_basics_19;
+            this.btnSearch.Location = new System.Drawing.Point(301, 38);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(35, 31);
+            this.btnSearch.TabIndex = 40;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 41;
+            this.label3.Text = "Active Status";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbStatus.DropDownWidth = 17;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(138, 110);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(121, 21);
+            this.cmbStatus.TabIndex = 42;
+            this.cmbStatus.ViewColumn = 0;
+            // 
+            // cmbDepartmentName
+            // 
+            this.cmbDepartmentName.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbDepartmentName.DropDownWidth = 17;
+            this.cmbDepartmentName.FormattingEnabled = true;
+            this.cmbDepartmentName.Location = new System.Drawing.Point(138, 11);
+            this.cmbDepartmentName.Name = "cmbDepartmentName";
+            this.cmbDepartmentName.Size = new System.Drawing.Size(198, 21);
+            this.cmbDepartmentName.TabIndex = 31;
+            this.cmbDepartmentName.ViewColumn = 0;
             // 
             // frmCategoryMasterDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(347, 339);
-            this.Controls.Add(this.btnRemove);
-            this.Controls.Add(this.btnAdd);
+            this.ClientSize = new System.Drawing.Size(351, 205);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSave);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtCategoryName);
@@ -179,7 +194,7 @@
             this.Name = "frmCategoryMasterDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Category Master Details";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.frmCategoryMasterDetails_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,18 +202,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCategoryName;
         private ColumnComboBox cmbDepartmentName;
         private System.Windows.Forms.TextBox txtCategoryCode;
         private System.Windows.Forms.Label lblDepartmentName;
-        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label3;
+        private ColumnComboBox cmbStatus;
 
     }
 }
--- RMS/RMS/Forms/frmCategoryMasterDetails.cs
+++ RMS/RMS/Forms/frmCategoryMasterDetails.cs
@@ -7,14 +7,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
+using BusinessObject;
 
 namespace RMS.Forms
 {
     public partial class frmCategoryMasterDetails : Form
     {
+        #region Objects
+        clsCommonMethods cCommonMethods = new clsCommonMethods();
+        clsStatusMaster cStatusMaster = new clsStatusMaster();
+        clsCategoryMaster cCategoryMaster = new clsCategoryMaster();
+        clsDepartment cDepartment = new clsDepartment();
+        clsGlobleVariable cGlobleVariable = new clsGlobleVariable();
+        objCategoryMaster oCategoryMaster = new objCategoryMaster();
+        #endregion
+
+        #region variables
+        int result = -1;
+        #endregion
+
         public frmCategoryMasterDetails()
         {
             InitializeComponent();
+        }
+
+        private void frmCategoryMasterDetails_Load(object sender, EventArgs e)
+        {
+            cCommonMethods.loadComboRMS(cDepartment.GetDepartmentData(cGlobleVariable.LocationCode), cmbDepartmentName, 2);
+            cCommonMethods.loadComboRMS(cStatusMaster.GetStatusDetails(), cmbStatus, 1);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            //if (ValidateData())
+            //{
+            //    oCategoryMaster = cCategoryMaster.GetCategoryData(cGlobleVariable.LocationCode, this.txtCategoryCode.Text);
+
+            //    if (oCategoryMaster.IsExistCategory == false)
+            //    {
+            //        result = InsertUpdateData();
+            //        if (result != -1)
+            //        {
+            //            MessageBox.Show("Successfully Saved...!", "Category", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //            clear();
+            //        }
+            //        else
+            //        {
+            //            MessageBox.Show("Category Data Not Saved...!", "Category", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Record already exist...!", "Category", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //    }
+            //}
         }
     }
 }
