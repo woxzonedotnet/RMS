7f54ad14fc2ff241e2d4a6d2f731ec53e0995ffe
dilshan8kt <dilshan.kt@gmail.com>
2017-07-05 16:02:04 +0530

    Add Status Class And Object

---
+++ RMS/BusinessLogic/clsStatusMaster.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using System.Data;
+using BusinessObject;
+
+namespace BusinessLogic
+{
+    public class clsStatusMaster
+    {
+        #region Objects
+        clsDBConnection cDBConnection = new clsDBConnection();
+        objStatusMaster oStatusMaster=new objStatusMaster();
+        #endregion
+
+        #region GetStatusDetails
+        public DataTable GetStatusDetails()
+        {
+            DataTable dtStatusMaster = cDBConnection.SearchData("tbl_StatusMaster");
+            return dtStatusMaster;
+        }
+        #endregion
+
+        #region GetStatusDetails using StatusCode
+        public string GetStatusByCode(int strStatusCode)
+        {
+            string strWhereString = "fldStatusCode ='" + strStatusCode + "'";
+
+            DataTable dtStatusMaster = cDBConnection.SearchData("tbl_StatusMaster", strWhereString);
+
+            if (dtStatusMaster.Rows.Count > 0)
+            {
+                oStatusMaster.StatusCode = Convert.ToInt16(dtStatusMaster.Rows[0][0]);
+                oStatusMaster.StatusName = dtStatusMaster.Rows[0][1].ToString();
+                oStatusMaster.IsExistStatus = true;
+            }
+            else
+            {
+                oStatusMaster.IsExistStatus = false;
+            }
+            return oStatusMaster.StatusName;
+        }
+        #endregion
+    }
+}
---
+++ RMS/BusinessObject/objStatusMaster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objStatusMaster
+    {
+        public int StatusCode { get; set; }
+        public string StatusName { get; set; }
+        public bool IsExistStatus { get; set; }
+    }
+}
