acbd95dc4acf82fcbd658b05e467abe547504fa1
dilshan8kt <dilshan.kt@gmail.com>
2017-07-05 12:25:17 +0530

    Brows data form add

---
+++ RMS/RMS/Forms/frmBrowsData.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace RMS.Forms
+{
+    partial class frmBrowsData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dgvDataView = new System.Windows.Forms.DataGridView();
+            this.txtSearchBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(378, 448);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(71, 23);
+            this.btnSelect.TabIndex = 26;
+            this.btnSelect.Text = "&Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnExit.FlatAppearance.BorderSize = 5;
+            this.btnExit.Location = new System.Drawing.Point(452, 448);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(83, 23);
+            this.btnExit.TabIndex = 27;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dgvDataView
+            // 
+            this.dgvDataView.AllowUserToAddRows = false;
+            this.dgvDataView.AllowUserToDeleteRows = false;
+            this.dgvDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDataView.Location = new System.Drawing.Point(10, 108);
+            this.dgvDataView.Name = "dgvDataView";
+            this.dgvDataView.ReadOnly = true;
+            this.dgvDataView.Size = new System.Drawing.Size(525, 334);
+            this.dgvDataView.TabIndex = 25;
+            this.dgvDataView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDataView_CellClick);
+            this.dgvDataView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDataView_CellDoubleClick);
+            this.dgvDataView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dgvDataView_KeyDown);
+            // 
+            // txtSearchBox
+            // 
+            this.txtSearchBox.Location = new System.Drawing.Point(10, 82);
+            this.txtSearchBox.Name = "txtSearchBox";
+            this.txtSearchBox.Size = new System.Drawing.Size(525, 20);
+            this.txtSearchBox.TabIndex = 24;
+            this.txtSearchBox.TextChanged += new System.EventHandler(this.txtSearchBox_TextChanged);
+            this.txtSearchBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtSearchBox_KeyDown);
+            // 
+            // frmBrowsData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 475);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.dgvDataView);
+            this.Controls.Add(this.txtSearchBox);
+            this.Name = "frmBrowsData";
+            this.Text = "frmBrowsData";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Button btnSelect;
+        public System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dgvDataView;
+        private System.Windows.Forms.TextBox txtSearchBox;
+    }
+}
