3cffe126493ec6178fdb654dbcf557d60ad2a096
dilshan8kt <dilshan.kt@gmail.com>
2017-07-05 17:20:49 +0530

    Add Category Object

---
+++ RMS/BusinessObject/objCategoryMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objCategoryMaster
+    {
+        public string LocationCode { get; set; }
+        public string DepartmentCode { get; set; }
+        public string CategoryCode { get; set; }
+        public string CategoryName { get; set; }
+        public int Status { get; set; }
+        public bool IsExistCategory { get; set; }
+    }
+}
