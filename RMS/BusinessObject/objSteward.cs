851c946dd8ef92679f9d4e4fff2c96f97a7429d6
dilshan8kt <dilshan.kt@gmail.com>
2017-07-06 14:53:53 +0530

    Add Steward Object

---
+++ RMS/BusinessObject/objSteward.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objSteward
+    {
+        public string LocationCode { get; set; }
+        public string StewardCode { get; set; }
+        public string StewardName { get; set; }
+        public string Password { get; set; }
+        public int Status { get; set; }
+        public bool IsExits { get; set; }
+    }
+}
