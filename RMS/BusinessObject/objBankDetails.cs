3053cb801aa4c872cdaceff6fdaa85f495ce0ae4
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-06 11:42:33 +0530

    Bank Details object class add

---
+++ RMS/BusinessObject/objBankDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objBankDetails
+    {
+        public string BankCode { get; set; }
+        public string BankName { get; set; }
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
