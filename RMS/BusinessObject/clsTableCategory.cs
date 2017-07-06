a61446fda5c3be80f8e6441f82244aaab566bf01
dilshan8kt <dilshan.kt@gmail.com>
2017-07-06 15:53:28 +0530

    Add Table Category Object

---
+++ RMS/BusinessObject/clsTableCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class clsTableCategory
+    {
+        public string LocationCode { get; set; }
+        public string TableCategoryCode { get; set; }
+        public string TableCategoryName { get; set; }
+
+    }
+}
