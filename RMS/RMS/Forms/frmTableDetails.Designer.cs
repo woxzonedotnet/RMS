b60e7ae3931447930d0ae3709862ffea5ed966fc
dilshan8kt <dilshan.kt@gmail.com>
2017-07-07 12:34:18 +0530

    Edit Table Master Form

--- RMS/RMS/Forms/frmTableDetails.Designer.cs
+++ RMS/RMS/Forms/frmTableDetails.Designer.cs
@@ -28,95 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTableDetails));
             this.btnSearch = new System.Windows.Forms.Button();
-            this.txtTableNo = new System.Windows.Forms.TextBox();
-            this.lblTableNo = new System.Windows.Forms.Label();
-            this.cmbTable = new RMS.ColumnComboBox();
-            this.cmbTableLocation = new RMS.ColumnComboBox();
-            this.txtNoOfSeats = new System.Windows.Forms.TextBox();
+            this.txtTableCode = new System.Windows.Forms.TextBox();
+            this.lblTableCode = new System.Windows.Forms.Label();
             this.lblTable = new System.Windows.Forms.Label();
             this.lblTableLocation = new System.Windows.Forms.Label();
             this.lblNoOfSeats = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.nudNoOfSeats = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errTable = new System.Windows.Forms.ErrorProvider(this.components);
+            this.cmbStatus = new RMS.ColumnComboBox();
+            this.cmbTableLocation = new RMS.ColumnComboBox();
+            this.cmbTableCategory = new RMS.ColumnComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNoOfSeats)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errTable)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSearch
             // 
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSearch.Image = global::RMS.Properties.Resources._1498666014_basics_19;
-            this.btnSearch.Location = new System.Drawing.Point(289, 7);
+            this.btnSearch.Location = new System.Drawing.Point(289, 41);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(35, 31);
             this.btnSearch.TabIndex = 20;
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
-            // txtTableNo
+            // txtTableCode
             // 
-            this.txtTableNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTableNo.Location = new System.Drawing.Point(133, 12);
-            this.txtTableNo.Name = "txtTableNo";
-            this.txtTableNo.Size = new System.Drawing.Size(150, 21);
-            this.txtTableNo.TabIndex = 19;
+            this.txtTableCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTableCode.Location = new System.Drawing.Point(133, 46);
+            this.txtTableCode.Name = "txtTableCode";
+            this.txtTableCode.Size = new System.Drawing.Size(150, 21);
+            this.txtTableCode.TabIndex = 19;
             // 
-            // lblTableNo
+            // lblTableCode
             // 
-            this.lblTableNo.AutoSize = true;
-            this.lblTableNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTableNo.Location = new System.Drawing.Point(23, 15);
-            this.lblTableNo.Name = "lblTableNo";
-            this.lblTableNo.Size = new System.Drawing.Size(65, 16);
-            this.lblTableNo.TabIndex = 18;
-            this.lblTableNo.Text = "Table No";
-            // 
-            // cmbTable
-            // 
-            this.cmbTable.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.cmbTable.DropDownWidth = 17;
-            this.cmbTable.FormattingEnabled = true;
-            this.cmbTable.Location = new System.Drawing.Point(133, 48);
-            this.cmbTable.Name = "cmbTable";
-            this.cmbTable.Size = new System.Drawing.Size(191, 21);
-            this.cmbTable.TabIndex = 21;
-            this.cmbTable.ViewColumn = 0;
-            // 
-            // cmbTableLocation
-            // 
-            this.cmbTableLocation.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
-            this.cmbTableLocation.DropDownWidth = 17;
-            this.cmbTableLocation.FormattingEnabled = true;
-            this.cmbTableLocation.Location = new System.Drawing.Point(133, 85);
-            this.cmbTableLocation.Name = "cmbTableLocation";
-            this.cmbTableLocation.Size = new System.Drawing.Size(191, 21);
-            this.cmbTableLocation.TabIndex = 22;
-            this.cmbTableLocation.ViewColumn = 0;
-            // 
-            // txtNoOfSeats
-            // 
-            this.txtNoOfSeats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNoOfSeats.Location = new System.Drawing.Point(133, 122);
-            this.txtNoOfSeats.Name = "txtNoOfSeats";
-            this.txtNoOfSeats.Size = new System.Drawing.Size(191, 21);
-            this.txtNoOfSeats.TabIndex = 23;
+            this.lblTableCode.AutoSize = true;
+            this.lblTableCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableCode.Location = new System.Drawing.Point(23, 47);
+            this.lblTableCode.Name = "lblTableCode";
+            this.lblTableCode.Size = new System.Drawing.Size(65, 16);
+            this.lblTableCode.TabIndex = 18;
+            this.lblTableCode.Text = "Table No";
             // 
             // lblTable
             // 
             this.lblTable.AutoSize = true;
             this.lblTable.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTable.Location = new System.Drawing.Point(23, 49);
+            this.lblTable.Location = new System.Drawing.Point(23, 13);
             this.lblTable.Name = "lblTable";
-            this.lblTable.Size = new System.Drawing.Size(44, 16);
+            this.lblTable.Size = new System.Drawing.Size(102, 16);
             this.lblTable.TabIndex = 24;
-            this.lblTable.Text = "Table";
+            this.lblTable.Text = "Table Category";
             // 
             // lblTableLocation
             // 
             this.lblTableLocation.AutoSize = true;
             this.lblTableLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTableLocation.Location = new System.Drawing.Point(23, 86);
+            this.lblTableLocation.Location = new System.Drawing.Point(23, 81);
             this.lblTableLocation.Name = "lblTableLocation";
             this.lblTableLocation.Size = new System.Drawing.Size(98, 16);
             this.lblTableLocation.TabIndex = 25;
@@ -126,7 +103,7 @@
             // 
             this.lblNoOfSeats.AutoSize = true;
             this.lblNoOfSeats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNoOfSeats.Location = new System.Drawing.Point(23, 125);
+            this.lblNoOfSeats.Location = new System.Drawing.Point(23, 114);
             this.lblNoOfSeats.Name = "lblNoOfSeats";
             this.lblNoOfSeats.Size = new System.Drawing.Size(80, 16);
             this.lblNoOfSeats.TabIndex = 26;
@@ -135,64 +112,142 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(257, 155);
+            this.btnClose.Location = new System.Drawing.Point(257, 183);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 36);
             this.btnClose.TabIndex = 29;
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // btnClear
             // 
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(176, 155);
+            this.btnClear.Location = new System.Drawing.Point(176, 183);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 36);
             this.btnClear.TabIndex = 28;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(14, 155);
+            this.btnSave.Location = new System.Drawing.Point(14, 183);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 36);
             this.btnSave.TabIndex = 27;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // button1
+            // btnUpdate
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(95, 155);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 36);
-            this.button1.TabIndex = 30;
-            this.button1.Text = "Save";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(95, 183);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 36);
+            this.btnUpdate.TabIndex = 30;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // nudNoOfSeats
+            // 
+            this.nudNoOfSeats.Location = new System.Drawing.Point(133, 114);
+            this.nudNoOfSeats.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudNoOfSeats.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNoOfSeats.Name = "nudNoOfSeats";
+            this.nudNoOfSeats.Size = new System.Drawing.Size(109, 20);
+            this.nudNoOfSeats.TabIndex = 31;
+            this.nudNoOfSeats.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 148);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Status";
+            // 
+            // errTable
+            // 
+            this.errTable.ContainerControl = this;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbStatus.DropDownWidth = 17;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(133, 147);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(109, 21);
+            this.cmbStatus.TabIndex = 33;
+            this.cmbStatus.ViewColumn = 0;
+            // 
+            // cmbTableLocation
+            // 
+            this.cmbTableLocation.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbTableLocation.DropDownWidth = 17;
+            this.cmbTableLocation.FormattingEnabled = true;
+            this.cmbTableLocation.Location = new System.Drawing.Point(133, 80);
+            this.cmbTableLocation.Name = "cmbTableLocation";
+            this.cmbTableLocation.Size = new System.Drawing.Size(191, 21);
+            this.cmbTableLocation.TabIndex = 22;
+            this.cmbTableLocation.ViewColumn = 0;
+            // 
+            // cmbTableCategory
+            // 
+            this.cmbTableCategory.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbTableCategory.DropDownWidth = 17;
+            this.cmbTableCategory.FormattingEnabled = true;
+            this.cmbTableCategory.Location = new System.Drawing.Point(133, 12);
+            this.cmbTableCategory.Name = "cmbTableCategory";
+            this.cmbTableCategory.Size = new System.Drawing.Size(191, 21);
+            this.cmbTableCategory.TabIndex = 21;
+            this.cmbTableCategory.ViewColumn = 0;
             // 
             // frmTableDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(346, 202);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(346, 228);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudNoOfSeats);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.lblNoOfSeats);
             this.Controls.Add(this.lblTableLocation);
             this.Controls.Add(this.lblTable);
-            this.Controls.Add(this.txtNoOfSeats);
             this.Controls.Add(this.cmbTableLocation);
-            this.Controls.Add(this.cmbTable);
+            this.Controls.Add(this.cmbTableCategory);
             this.Controls.Add(this.btnSearch);
-            this.Controls.Add(this.txtTableNo);
-            this.Controls.Add(this.lblTableNo);
+            this.Controls.Add(this.txtTableCode);
+            this.Controls.Add(this.lblTableCode);
             this.Name = "frmTableDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Table Details";
+            this.Load += new System.EventHandler(this.frmTableDetails_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNoOfSeats)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -201,17 +256,20 @@
         #endregion
 
         private System.Windows.Forms.Button btnSearch;
-        private System.Windows.Forms.TextBox txtTableNo;
-        private System.Windows.Forms.Label lblTableNo;
-        private ColumnComboBox cmbTable;
+        private System.Windows.Forms.TextBox txtTableCode;
+        private System.Windows.Forms.Label lblTableCode;
+        private ColumnComboBox cmbTableCategory;
         private ColumnComboBox cmbTableLocation;
-        private System.Windows.Forms.TextBox txtNoOfSeats;
         private System.Windows.Forms.Label lblTable;
         private System.Windows.Forms.Label lblTableLocation;
         private System.Windows.Forms.Label lblNoOfSeats;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.NumericUpDown nudNoOfSeats;
+        private System.Windows.Forms.Label label1;
+        private ColumnComboBox cmbStatus;
+        private System.Windows.Forms.ErrorProvider errTable;
     }
 }
--- RMS/RMS/Forms/frmTableDetails.cs
+++ RMS/RMS/Forms/frmTableDetails.cs
@@ -7,14 +7,210 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessObject;
+using BusinessLogic;
 
 namespace RMS.Forms
 {
     public partial class frmTableDetails : Form
     {
+        #region Objects
+        clsCommonMethods cCommonMethods = new clsCommonMethods();
+        clsStatusMaster cStatusMaster = new clsStatusMaster();
+        clsTableCategory cTableCategory = new clsTableCategory();
+        clsGlobleVariable cGlobleVariable = new clsGlobleVariable();
+        clsSubLocation cSubLocation = new clsSubLocation();
+        objTableMaster oTableMaster = new objTableMaster();
+        clsTableMaster cTableMaster = new clsTableMaster();
+        #endregion
+
+        #region Variable
+        int result = -1;
+        #endregion
+
         public frmTableDetails()
         {
             InitializeComponent();
+        }
+
+        private void frmTableDetails_Load(object sender, EventArgs e)
+        {
+            cCommonMethods.loadComboRMS(cStatusMaster.GetStatusDetails(),cmbStatus,1);
+            cCommonMethods.loadComboRMS(cTableCategory.GetTableCategoryData(cGlobleVariable.LocationCode),cmbTableCategory,2);
+            cCommonMethods.loadComboRMS(cSubLocation.GetSubLocationData(cGlobleVariable.LocationCode),cmbTableLocation,2);
+            this.btnUpdate.Enabled = false;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ValidateData())
+            {
+                oTableMaster = cTableMaster.GetTableMasterData(cGlobleVariable.LocationCode, this.cmbTableCategory["fldTableCategoryCode"].ToString(),this.txtTableCode.Text);
+
+                if (oTableMaster.IsExists == false)
+                {
+                    result = InsertUpdateData();
+                    if (result != -1)
+                    {
+                        MessageBox.Show("Successfully Saved...!", "Table", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Table Category Data Not Saved...!", "Table", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record already exist...!", "Table", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        #region InsertUpdate TableMaster Data
+        private int InsertUpdateData()
+        {
+            oTableMaster.LocationCode = cGlobleVariable.LocationCode.ToString();
+            oTableMaster.TableCategoryCode = this.cmbTableCategory["fldTableCategoryCode"].ToString();
+            oTableMaster.TableCode = this.txtTableCode.Text;
+            oTableMaster.SubLocationCode = this.cmbTableLocation["fldSubLocationCode"].ToString();
+            oTableMaster.TableSeats = Convert.ToInt16(this.nudNoOfSeats.Value);
+            oTableMaster.TableStatus = Convert.ToInt16(cmbStatus["fldStatusCode"]);
+
+            return cTableMaster.InsertUpdateData(oTableMaster);
+        }
+        #endregion
+
+        public bool ValidateData()
+        {
+            bool isValidate = true;
+
+            if (cmbTableCategory.SelectedIndex == -1)
+            {
+                errTable.SetError(cmbTableCategory, "Please Select Table Category");
+                isValidate = false;
+            }
+            else
+            {
+                errTable.SetError(cmbTableCategory, "");
+
+            }
+
+            if (txtTableCode.Text == "")
+            {
+                errTable.SetError(txtTableCode, "Please Enter Table No");
+                isValidate = false;
+            }
+            else
+            {
+                errTable.SetError(txtTableCode, "");
+            }
+
+            if (cmbTableLocation.SelectedIndex == -1)
+            {
+                errTable.SetError(cmbTableLocation, "Please Select Table Location");
+                isValidate = false;
+            }
+            else
+            {
+                errTable.SetError(cmbTableLocation, "");
+            }
+
+            if (cmbStatus.SelectedIndex == -1)
+            {
+                errTable.SetError(cmbStatus, "Please Select Status");
+                isValidate = false;
+            }
+            else
+            {
+                errTable.SetError(cmbStatus, "");
+            }
+
+            return isValidate;
+        }
+
+        public void clear()
+        {
+            cCommonMethods.ClearForm(this);
+            this.txtTableCode.Enabled = true;
+            this.cmbTableCategory.Enabled = true;
+            this.btnSave.Enabled = true;
+            this.btnUpdate.Enabled = false;
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (cmbTableCategory.SelectedIndex != -1)
+            {
+                string[] strFieldList = new string[2];
+                strFieldList[0] = "fldTableCode";
+                strFieldList[1] = "fldTableSeats";
+
+                string[] strHeaderList = new string[2];
+                strHeaderList[0] = "Table Code";
+                strHeaderList[1] = "Table Seats";
+
+                int[] iHeaderWidth = new int[2];
+                iHeaderWidth[0] = 150;
+                iHeaderWidth[1] = 320;
+
+                string strReturnString = "Table Code";
+                string strWhere = "fldLocationCode= '" + cGlobleVariable.LocationCode + "' and fldTableCategoryCode='" + cmbTableCategory["fldTableCategoryCode"] + "'";
+                txtTableCode.Text = cCommonMethods.BrowsData("tbl_TableMaster", strFieldList, strHeaderList, iHeaderWidth, strReturnString, strWhere, "Table Master");
+                if (txtTableCode.Text != "")
+                {
+                    LoadTableMaster();
+                }
+            }
+            else 
+            {
+                MessageBox.Show("Please Select Table Category");
+            }
+        }
+
+        public void LoadTableMaster()
+        {
+            oTableMaster = cTableMaster.GetTableMasterData(cGlobleVariable.LocationCode, this.cmbTableCategory["fldTableCategoryCode"].ToString(),this.txtTableCode.Text);
+            
+            cmbTableLocation.SetText(cSubLocation.GetSubLocationData(cGlobleVariable.LocationCode, oTableMaster.SubLocationCode).SubLocationName);
+            cmbStatus.SetText(cStatusMaster.GetStatusByCode(oTableMaster.TableStatus));
+            nudNoOfSeats.Value = oTableMaster.TableSeats;
+
+            this.cmbTableCategory.Enabled = false;
+            this.txtTableCode.Enabled = false;
+            this.btnSave.Enabled = false;
+            this.btnUpdate.Enabled = true;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (ValidateData())
+            {
+                oTableMaster = cTableMaster.GetTableMasterData(cGlobleVariable.LocationCode, this.cmbTableCategory["fldTableCategoryCode"].ToString(), this.txtTableCode.Text);
+
+                if (oTableMaster.IsExists == true)
+                {
+                    result = InsertUpdateData();
+                    if (result != -1)
+                    {
+                        MessageBox.Show("Successfully Updated...!", "Table", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Table Category Data Not Update...!", "Table", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
         }
     }
 }
