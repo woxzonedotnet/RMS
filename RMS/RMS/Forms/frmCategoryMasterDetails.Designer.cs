87d06fe83570a508d5644037d58b5f1916d79f83
dilshan8kt <dilshan.kt@gmail.com>
2017-07-07 14:39:42 +0530

    Edit Category Master Form

--- RMS/RMS/Forms/frmCategoryMasterDetails.Designer.cs
+++ RMS/RMS/Forms/frmCategoryMasterDetails.Designer.cs
@@ -44,13 +44,22 @@
             this.errCategory = new System.Windows.Forms.ErrorProvider(this.components);
             this.cmbStatus = new RMS.ColumnComboBox();
             this.cmbDepartmentName = new RMS.ColumnComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblDepartmentTitle = new System.Windows.Forms.Label();
+            this.lblTableCategoryTitle = new System.Windows.Forms.Label();
+            this.lblFormTitle = new System.Windows.Forms.Label();
+            this.lblCategoryTitle = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.errCategory)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnUpdate
             // 
             this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnUpdate.Location = new System.Drawing.Point(97, 157);
+            this.btnUpdate.Location = new System.Drawing.Point(97, 195);
             this.btnUpdate.Name = "btnUpdate";
             this.btnUpdate.Size = new System.Drawing.Size(75, 36);
             this.btnUpdate.TabIndex = 39;
@@ -61,7 +70,7 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(260, 157);
+            this.btnClose.Location = new System.Drawing.Point(260, 195);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(75, 36);
             this.btnClose.TabIndex = 38;
@@ -72,7 +81,7 @@
             // btnClear
             // 
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(179, 157);
+            this.btnClear.Location = new System.Drawing.Point(179, 195);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 36);
             this.btnClear.TabIndex = 37;
@@ -83,7 +92,7 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(16, 157);
+            this.btnSave.Location = new System.Drawing.Point(16, 195);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 36);
             this.btnSave.TabIndex = 36;
@@ -95,7 +104,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 80);
+            this.label2.Location = new System.Drawing.Point(14, 118);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(103, 16);
             this.label2.TabIndex = 34;
@@ -105,7 +114,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(14, 47);
+            this.label1.Location = new System.Drawing.Point(14, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 16);
             this.label1.TabIndex = 33;
@@ -114,7 +123,7 @@
             // txtCategoryName
             // 
             this.txtCategoryName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategoryName.Location = new System.Drawing.Point(138, 77);
+            this.txtCategoryName.Location = new System.Drawing.Point(138, 115);
             this.txtCategoryName.Name = "txtCategoryName";
             this.txtCategoryName.Size = new System.Drawing.Size(198, 21);
             this.txtCategoryName.TabIndex = 32;
@@ -122,7 +131,7 @@
             // txtCategoryCode
             // 
             this.txtCategoryCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCategoryCode.Location = new System.Drawing.Point(138, 44);
+            this.txtCategoryCode.Location = new System.Drawing.Point(138, 82);
             this.txtCategoryCode.Name = "txtCategoryCode";
             this.txtCategoryCode.Size = new System.Drawing.Size(157, 21);
             this.txtCategoryCode.TabIndex = 30;
@@ -131,7 +140,7 @@
             // 
             this.lblDepartmentName.AutoSize = true;
             this.lblDepartmentName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDepartmentName.Location = new System.Drawing.Point(14, 12);
+            this.lblDepartmentName.Location = new System.Drawing.Point(14, 50);
             this.lblDepartmentName.Name = "lblDepartmentName";
             this.lblDepartmentName.Size = new System.Drawing.Size(118, 16);
             this.lblDepartmentName.TabIndex = 29;
@@ -141,7 +150,7 @@
             // 
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSearch.Image = global::RMS.Properties.Resources._1498666014_basics_19;
-            this.btnSearch.Location = new System.Drawing.Point(301, 38);
+            this.btnSearch.Location = new System.Drawing.Point(301, 76);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(35, 31);
             this.btnSearch.TabIndex = 40;
@@ -152,7 +161,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(14, 111);
+            this.label3.Location = new System.Drawing.Point(14, 149);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(85, 16);
             this.label3.TabIndex = 41;
@@ -167,7 +176,7 @@
             this.cmbStatus.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
             this.cmbStatus.DropDownWidth = 17;
             this.cmbStatus.FormattingEnabled = true;
-            this.cmbStatus.Location = new System.Drawing.Point(138, 110);
+            this.cmbStatus.Location = new System.Drawing.Point(138, 148);
             this.cmbStatus.Name = "cmbStatus";
             this.cmbStatus.Size = new System.Drawing.Size(121, 21);
             this.cmbStatus.TabIndex = 42;
@@ -178,17 +187,100 @@
             this.cmbDepartmentName.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
             this.cmbDepartmentName.DropDownWidth = 17;
             this.cmbDepartmentName.FormattingEnabled = true;
-            this.cmbDepartmentName.Location = new System.Drawing.Point(138, 11);
+            this.cmbDepartmentName.Location = new System.Drawing.Point(138, 49);
             this.cmbDepartmentName.Name = "cmbDepartmentName";
             this.cmbDepartmentName.Size = new System.Drawing.Size(198, 21);
             this.cmbDepartmentName.TabIndex = 31;
             this.cmbDepartmentName.ViewColumn = 0;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.panel1.Controls.Add(this.lblCategoryTitle);
+            this.panel1.Controls.Add(this.lblDepartmentTitle);
+            this.panel1.Controls.Add(this.lblTableCategoryTitle);
+            this.panel1.Controls.Add(this.lblFormTitle);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(351, 31);
+            this.panel1.TabIndex = 43;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            // 
+            // lblDepartmentTitle
+            // 
+            this.lblDepartmentTitle.AutoSize = true;
+            this.lblDepartmentTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDepartmentTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblDepartmentTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblDepartmentTitle.Name = "lblDepartmentTitle";
+            this.lblDepartmentTitle.Size = new System.Drawing.Size(0, 16);
+            this.lblDepartmentTitle.TabIndex = 2;
+            // 
+            // lblTableCategoryTitle
+            // 
+            this.lblTableCategoryTitle.AutoSize = true;
+            this.lblTableCategoryTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTableCategoryTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTableCategoryTitle.Location = new System.Drawing.Point(13, 9);
+            this.lblTableCategoryTitle.Name = "lblTableCategoryTitle";
+            this.lblTableCategoryTitle.Size = new System.Drawing.Size(0, 16);
+            this.lblTableCategoryTitle.TabIndex = 1;
+            // 
+            // lblFormTitle
+            // 
+            this.lblFormTitle.AutoSize = true;
+            this.lblFormTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFormTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblFormTitle.Location = new System.Drawing.Point(13, 9);
+            this.lblFormTitle.Name = "lblFormTitle";
+            this.lblFormTitle.Size = new System.Drawing.Size(0, 16);
+            this.lblFormTitle.TabIndex = 0;
+            // 
+            // lblCategoryTitle
+            // 
+            this.lblCategoryTitle.AutoSize = true;
+            this.lblCategoryTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoryTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCategoryTitle.Location = new System.Drawing.Point(15, 9);
+            this.lblCategoryTitle.Name = "lblCategoryTitle";
+            this.lblCategoryTitle.Size = new System.Drawing.Size(0, 16);
+            this.lblCategoryTitle.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.panel3.Location = new System.Drawing.Point(-9, 30);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(10, 215);
+            this.panel3.TabIndex = 44;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.panel2.Location = new System.Drawing.Point(350, 31);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 215);
+            this.panel2.TabIndex = 45;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.panel4.Location = new System.Drawing.Point(0, 245);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(351, 10);
+            this.panel4.TabIndex = 45;
+            // 
             // frmCategoryMasterDetails
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(351, 205);
+            this.ClientSize = new System.Drawing.Size(351, 246);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.cmbStatus);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.btnSearch);
@@ -202,11 +294,14 @@
             this.Controls.Add(this.cmbDepartmentName);
             this.Controls.Add(this.txtCategoryCode);
             this.Controls.Add(this.lblDepartmentName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmCategoryMasterDetails";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Category Master Details";
             this.Load += new System.EventHandler(this.frmCategoryMasterDetails_Load);
             ((System.ComponentModel.ISupportInitialize)(this.errCategory)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -228,6 +323,14 @@
         private System.Windows.Forms.Label label3;
         private ColumnComboBox cmbStatus;
         private System.Windows.Forms.ErrorProvider errCategory;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCategoryTitle;
+        private System.Windows.Forms.Label lblDepartmentTitle;
+        private System.Windows.Forms.Label lblTableCategoryTitle;
+        private System.Windows.Forms.Label lblFormTitle;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
 
     }
 }
--- RMS/RMS/Forms/frmCategoryMasterDetails.cs
+++ RMS/RMS/Forms/frmCategoryMasterDetails.cs
@@ -25,11 +25,13 @@ namespace RMS.Forms
 
         #region variables
         int result = -1;
+        Point lastClick;
         #endregion
 
         public frmCategoryMasterDetails()
         {
             InitializeComponent();
+            this.lblCategoryTitle.Text = this.Text;
         }
 
         private void frmCategoryMasterDetails_Load(object sender, EventArgs e)
@@ -215,6 +217,20 @@ namespace RMS.Forms
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Dispose();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastClick.X;
+                this.Top += e.Y - lastClick.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastClick = e.Location;
         }
     }
 }
