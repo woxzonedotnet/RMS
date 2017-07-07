7a145c49862696055dc7c6d6c9a3fffa810a6295
dilshan8kt <dilshan.kt@gmail.com>
2017-07-07 12:35:57 +0530

    Add Table Master Object

---
+++ RMS/BusinessObject/objTableMaster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objTableMaster
+    {
+        public string LocationCode { get; set; }
+        public string TableCode { get; set; }
+        public string TableCategoryCode { get; set; }
+        public string SubLocationCode { get; set; }
+        public int TableSeats { get; set; }
+        public int TableStatus { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
