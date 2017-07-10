30a601fc652a45ab681cd3966862cbe695edf067
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-10 14:08:46 +0530

    BankCard form Edit

--- RMS/RMS/Forms/frmBankCards.Designer.cs
+++ RMS/RMS/Forms/frmBankCards.Designer.cs
@@ -28,29 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBankCards));
             this.lblCardCode = new System.Windows.Forms.Label();
             this.lblCardName = new System.Windows.Forms.Label();
-            this.lblShowOrder = new System.Windows.Forms.Label();
+            this.lblCardType = new System.Windows.Forms.Label();
             this.lblCurrency = new System.Windows.Forms.Label();
             this.lblCommision = new System.Windows.Forms.Label();
             this.lblPresentage = new System.Windows.Forms.Label();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtCardCode = new System.Windows.Forms.TextBox();
-            this.txtCardName = new System.Windows.Forms.TextBox();
-            this.txtShowOrder = new System.Windows.Forms.TextBox();
             this.txtCommision = new System.Windows.Forms.TextBox();
-            this.cmbCurrency = new RMS.ColumnComboBox();
+            this.lblActiveStatus = new System.Windows.Forms.Label();
             this.btnClose = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
+            this.errBankCard = new System.Windows.Forms.ErrorProvider(this.components);
+            this.cmbCardType = new RMS.ColumnComboBox();
+            this.cmbStatus = new RMS.ColumnComboBox();
+            this.cmbBankName = new RMS.ColumnComboBox();
+            this.cmbCurrency = new RMS.ColumnComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errBankCard)).BeginInit();
             this.SuspendLayout();
             // 
             // lblCardCode
             // 
             this.lblCardCode.AutoSize = true;
             this.lblCardCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblCardCode.Location = new System.Drawing.Point(43, 26);
+            this.lblCardCode.Location = new System.Drawing.Point(35, 26);
             this.lblCardCode.Name = "lblCardCode";
             this.lblCardCode.Size = new System.Drawing.Size(73, 16);
             this.lblCardCode.TabIndex = 0;
@@ -60,27 +66,27 @@
             // 
             this.lblCardName.AutoSize = true;
             this.lblCardName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblCardName.Location = new System.Drawing.Point(43, 62);
+            this.lblCardName.Location = new System.Drawing.Point(35, 62);
             this.lblCardName.Name = "lblCardName";
-            this.lblCardName.Size = new System.Drawing.Size(77, 16);
+            this.lblCardName.Size = new System.Drawing.Size(79, 16);
             this.lblCardName.TabIndex = 1;
-            this.lblCardName.Text = "Card Name";
+            this.lblCardName.Text = "Bank Name";
             // 
-            // lblShowOrder
+            // lblCardType
             // 
-            this.lblShowOrder.AutoSize = true;
-            this.lblShowOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblShowOrder.Location = new System.Drawing.Point(43, 98);
-            this.lblShowOrder.Name = "lblShowOrder";
-            this.lblShowOrder.Size = new System.Drawing.Size(78, 16);
-            this.lblShowOrder.TabIndex = 2;
-            this.lblShowOrder.Text = "Show Order";
+            this.lblCardType.AutoSize = true;
+            this.lblCardType.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblCardType.Location = new System.Drawing.Point(35, 98);
+            this.lblCardType.Name = "lblCardType";
+            this.lblCardType.Size = new System.Drawing.Size(72, 16);
+            this.lblCardType.TabIndex = 2;
+            this.lblCardType.Text = "Card Type";
             // 
             // lblCurrency
             // 
             this.lblCurrency.AutoSize = true;
             this.lblCurrency.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblCurrency.Location = new System.Drawing.Point(43, 135);
+            this.lblCurrency.Location = new System.Drawing.Point(35, 135);
             this.lblCurrency.Name = "lblCurrency";
             this.lblCurrency.Size = new System.Drawing.Size(61, 16);
             this.lblCurrency.TabIndex = 3;
@@ -90,7 +96,7 @@
             // 
             this.lblCommision.AutoSize = true;
             this.lblCommision.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.lblCommision.Location = new System.Drawing.Point(43, 173);
+            this.lblCommision.Location = new System.Drawing.Point(35, 173);
             this.lblCommision.Name = "lblCommision";
             this.lblCommision.Size = new System.Drawing.Size(75, 16);
             this.lblCommision.TabIndex = 4;
@@ -115,6 +121,7 @@
             this.btnSearch.Size = new System.Drawing.Size(35, 31);
             this.btnSearch.TabIndex = 10;
             this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // txtCardCode
             // 
@@ -123,26 +130,107 @@
             this.txtCardCode.Size = new System.Drawing.Size(182, 20);
             this.txtCardCode.TabIndex = 11;
             // 
-            // txtCardName
-            // 
-            this.txtCardName.Location = new System.Drawing.Point(132, 61);
-            this.txtCardName.Name = "txtCardName";
-            this.txtCardName.Size = new System.Drawing.Size(182, 20);
-            this.txtCardName.TabIndex = 12;
-            // 
-            // txtShowOrder
-            // 
-            this.txtShowOrder.Location = new System.Drawing.Point(132, 97);
-            this.txtShowOrder.Name = "txtShowOrder";
-            this.txtShowOrder.Size = new System.Drawing.Size(182, 20);
-            this.txtShowOrder.TabIndex = 13;
-            // 
             // txtCommision
             // 
             this.txtCommision.Location = new System.Drawing.Point(132, 172);
             this.txtCommision.Name = "txtCommision";
             this.txtCommision.Size = new System.Drawing.Size(182, 20);
             this.txtCommision.TabIndex = 14;
+            // 
+            // lblActiveStatus
+            // 
+            this.lblActiveStatus.AutoSize = true;
+            this.lblActiveStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblActiveStatus.Location = new System.Drawing.Point(35, 210);
+            this.lblActiveStatus.Name = "lblActiveStatus";
+            this.lblActiveStatus.Size = new System.Drawing.Size(85, 16);
+            this.lblActiveStatus.TabIndex = 53;
+            this.lblActiveStatus.Text = "Active Status";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnClose.Location = new System.Drawing.Point(280, 245);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 36);
+            this.btnClose.TabIndex = 57;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnClear.Location = new System.Drawing.Point(199, 245);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 36);
+            this.btnClear.TabIndex = 56;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnUpdate.Location = new System.Drawing.Point(118, 245);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 36);
+            this.btnUpdate.TabIndex = 55;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.btnSave.Location = new System.Drawing.Point(37, 245);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 36);
+            this.btnSave.TabIndex = 54;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // errBankCard
+            // 
+            this.errBankCard.ContainerControl = this;
+            // 
+            // cmbCardType
+            // 
+            this.cmbCardType.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbCardType.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbCardType.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbCardType.DropDownWidth = 17;
+            this.cmbCardType.FormattingEnabled = true;
+            this.cmbCardType.Location = new System.Drawing.Point(132, 97);
+            this.cmbCardType.Name = "cmbCardType";
+            this.cmbCardType.Size = new System.Drawing.Size(182, 21);
+            this.cmbCardType.TabIndex = 58;
+            this.cmbCardType.ViewColumn = 0;
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbStatus.DropDownWidth = 17;
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(132, 209);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(131, 21);
+            this.cmbStatus.TabIndex = 52;
+            this.cmbStatus.ViewColumn = 0;
+            // 
+            // cmbBankName
+            // 
+            this.cmbBankName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.cmbBankName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cmbBankName.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.cmbBankName.DropDownWidth = 17;
+            this.cmbBankName.FormattingEnabled = true;
+            this.cmbBankName.Location = new System.Drawing.Point(132, 61);
+            this.cmbBankName.Name = "cmbBankName";
+            this.cmbBankName.Size = new System.Drawing.Size(182, 21);
+            this.cmbBankName.TabIndex = 19;
+            this.cmbBankName.ViewColumn = 0;
             // 
             // cmbCurrency
             // 
@@ -155,59 +243,34 @@
             this.cmbCurrency.TabIndex = 15;
             this.cmbCurrency.ViewColumn = 0;
             // 
-            // btnClose
-            // 
-            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClose.Location = new System.Drawing.Point(280, 208);
-            this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(75, 36);
-            this.btnClose.TabIndex = 18;
-            this.btnClose.Text = "Close";
-            this.btnClose.UseVisualStyleBackColor = true;
-            // 
-            // btnClear
-            // 
-            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(199, 208);
-            this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(75, 36);
-            this.btnClear.TabIndex = 17;
-            this.btnClear.Text = "Clear";
-            this.btnClear.UseVisualStyleBackColor = true;
-            // 
-            // btnSave
-            // 
-            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(118, 208);
-            this.btnSave.Name = "btnSave";
-            this.btnSave.Size = new System.Drawing.Size(75, 36);
-            this.btnSave.TabIndex = 16;
-            this.btnSave.Text = "Save";
-            this.btnSave.UseVisualStyleBackColor = true;
-            // 
             // frmBankCards
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 257);
+            this.ClientSize = new System.Drawing.Size(392, 297);
+            this.Controls.Add(this.cmbCardType);
             this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
             this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblActiveStatus);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.cmbBankName);
             this.Controls.Add(this.cmbCurrency);
             this.Controls.Add(this.txtCommision);
-            this.Controls.Add(this.txtShowOrder);
-            this.Controls.Add(this.txtCardName);
             this.Controls.Add(this.txtCardCode);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.lblPresentage);
             this.Controls.Add(this.lblCommision);
             this.Controls.Add(this.lblCurrency);
-            this.Controls.Add(this.lblShowOrder);
+            this.Controls.Add(this.lblCardType);
             this.Controls.Add(this.lblCardName);
             this.Controls.Add(this.lblCardCode);
             this.Name = "frmBankCards";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Bank Cards";
+            this.Load += new System.EventHandler(this.frmBankCards_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errBankCard)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,18 +280,22 @@
 
         private System.Windows.Forms.Label lblCardCode;
         private System.Windows.Forms.Label lblCardName;
-        private System.Windows.Forms.Label lblShowOrder;
+        private System.Windows.Forms.Label lblCardType;
         private System.Windows.Forms.Label lblCurrency;
         private System.Windows.Forms.Label lblCommision;
         private System.Windows.Forms.Label lblPresentage;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtCardCode;
-        private System.Windows.Forms.TextBox txtCardName;
-        private System.Windows.Forms.TextBox txtShowOrder;
         private System.Windows.Forms.TextBox txtCommision;
         private ColumnComboBox cmbCurrency;
+        private ColumnComboBox cmbBankName;
+        private System.Windows.Forms.Label lblActiveStatus;
+        private ColumnComboBox cmbStatus;
         private System.Windows.Forms.Button btnClose;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ErrorProvider errBankCard;
+        private ColumnComboBox cmbCardType;
     }
 }
--- RMS/RMS/Forms/frmBankCards.cs
+++ RMS/RMS/Forms/frmBankCards.cs
@@ -7,14 +7,237 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
+using BusinessObject;
 
 namespace RMS.Forms
 {
     public partial class frmBankCards : Form
     {
+        #region Object
+        objBankCards oBankCards = new objBankCards();
+        clsBankCards cBankCards = new clsBankCards();
+        clsBankDetails cBankDetails = new clsBankDetails();
+        clsBankCardType cBankCardType = new clsBankCardType();
+        clsCommonMethods cCommonMethods = new clsCommonMethods();
+        clsStatusMaster cStatusMaster = new clsStatusMaster();
+        clsCurrencyMaster cCurrencyMaster = new clsCurrencyMaster();
+        #endregion
+
+        #region Variables
+        int result;
+        #endregion
+
         public frmBankCards()
         {
             InitializeComponent();
         }
+
+        #region Validate Bank Data
+        private bool ValidateData()
+        {
+            bool isValidate = true;
+
+            if (txtCardCode.Text == "")
+            {
+                errBankCard.SetError(txtCardCode, "Please Enter Card Code");
+                isValidate = false;
+            }
+            else
+            {
+                errBankCard.SetError(txtCardCode, "");
+
+            }
+
+            if (txtCommision.Text == "")
+            {
+                errBankCard.SetError(txtCommision, "Please Enter Commision");
+                isValidate = false;
+            }
+            else
+            {
+                errBankCard.SetError(txtCommision, "");
+
+            }
+
+            if (cmbBankName.SelectedIndex == -1)
+            {
+                errBankCard.SetError(cmbBankName, "Please Select Bank");
+                isValidate = false;
+            }
+            else
+            {
+                errBankCard.SetError(cmbBankName, "");
+            }
+
+            if (cmbCardType.SelectedIndex == -1)
+            {
+                errBankCard.SetError(cmbCardType, "Please Select Card Type");
+                isValidate = false;
+            }
+            else
+            {
+                errBankCard.SetError(cmbCardType, "");
+            }
+
+            if (cmbCurrency.SelectedIndex == -1)
+            {
+                errBankCard.SetError(cmbCurrency, "Please Select Currency");
+                isValidate = false;
+            }
+            else
+            {
+                errBankCard.SetError(cmbCurrency, "");
+            }
+
+            if (cmbStatus.SelectedIndex == -1)
+            {
+                errBankCard.SetError(cmbStatus, "Please Select Status");
+                isValidate = false;
+            }
+            else
+            {
+                errBankCard.SetError(cmbStatus, "");
+            }
+            return isValidate;
+        }
+        #endregion
+
+        #region Clear
+        public void clear()
+        {
+            cCommonMethods.ClearForm(this);
+            this.txtCardCode.Enabled = true;
+            this.btnSave.Enabled = true;
+            this.btnUpdate.Enabled = false;
+        }
+        #endregion
+
+        #region InsertUpdate BankCard Data
+        private int InsertUpdateData()
+        {
+            oBankCards.CardCode = txtCardCode.Text;
+            oBankCards.BankCode = cmbBankName["fldBankCode"].ToString();
+            oBankCards.CardType = cmbCardType["fldCardTypeCode"].ToString();
+            oBankCards.Currency = cmbCurrency["fldCurrencyCode"].ToString();
+            oBankCards.Commision = Convert.ToDouble(txtCommision.Text);
+            oBankCards.Status = Convert.ToInt16(cmbStatus["fldStatusCode"]);
+
+            return cBankCards.InsertUpdateData(oBankCards);
+        }
+        #endregion
+
+        #region Load Bank Cards
+        private void LoadBankCards()
+        {
+            oBankCards = cBankCards.GetBankCardData(txtCardCode.Text);
+            cmbBankName.SetText(cBankDetails.GetBankName(oBankCards.BankCode));
+            cmbCardType.SetText(cBankCardType.GetBankCardTypeByCode(oBankCards.CardType));
+            cmbCurrency.SetText(cCurrencyMaster.GetCurrencyByCode(oBankCards.Currency));
+            txtCommision.Text = oBankCards.Commision.ToString();
+            cmbStatus.SetText(cStatusMaster.GetStatusByCode(oBankCards.Status));
+
+            this.cmbBankName.Select();
+            this.txtCardCode.Enabled = false;
+            this.btnSave.Enabled = false;
+            this.btnUpdate.Enabled = true;
+        }
+        #endregion
+
+        private void frmBankCards_Load(object sender, EventArgs e)
+        {
+            cCommonMethods.loadComboRMS(cStatusMaster.GetStatusDetails(), cmbStatus, 1);
+            cCommonMethods.loadComboRMS(cBankDetails.GetBankData(), cmbBankName, 1);
+            cCommonMethods.loadComboRMS(cBankCardType.GetBankCardType(), cmbCardType, 1);
+            cCommonMethods.loadComboRMS(cCurrencyMaster.GetCurrencyDetails(), cmbCurrency, 1);
+            clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string[] strFieldList = new string[3];
+            strFieldList[0] = "fldCardCode";
+            strFieldList[1] = "fldBankCode";
+            strFieldList[2] = "fldCardTypeCode";
+
+            string[] strHeaderList = new string[3];
+            strHeaderList[0] = "Card Code";
+            strHeaderList[1] = "Bank Code";
+            strHeaderList[2] = "Card Type Code";
+
+            int[] iHeaderWidth = new int[3];
+            iHeaderWidth[0] = 150;
+            iHeaderWidth[1] = 150;
+            iHeaderWidth[2] = 320;
+
+            string strReturnString = "Card Code";
+            string strWhere = "fldStatus LIKE '%'";
+            txtCardCode.Text = cCommonMethods.BrowsData("tbl_BankCardMaster", strFieldList, strHeaderList, iHeaderWidth, strReturnString, strWhere, "Card Code");
+            if (txtCardCode.Text != "")
+            {
+                LoadBankCards();
+            }
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ValidateData())
+            {
+                oBankCards = cBankCards.GetBankCardData(txtCardCode.Text);
+
+                if (oBankCards.IsExists == false)
+                {
+                    result = InsertUpdateData();
+                    if (result != -1)
+                    {
+                        MessageBox.Show("Successfully Saved...!", "Bank Card", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data not Saved...!", "Bank Card", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Record already exist...!", "Bank Card", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (ValidateData())
+            {
+                oBankCards = cBankCards.GetBankCardData(txtCardCode.Text);
+
+                if (oBankCards.IsExists == true)
+                {
+                    result = InsertUpdateData();
+                    if (result != -1)
+                    {
+                        MessageBox.Show("Successfully Updated...!", "Bank Card", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Data not Update...!", "Bank Card", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        
+
+
     }
 }
