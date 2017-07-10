b61a631917de8b86018f5ad822f978ebf1f795e6
dilshan8kt <dilshan.kt@gmail.com>
2017-07-10 15:59:32 +0530

    Main Structure

--- RMS/RMS/Forms/frmMenuCategoryDetails.Designer.cs
+++ RMS/RMS/Forms/frmMenuCategoryDetails.Designer.cs
@@ -49,7 +49,7 @@
             // btnSearch
             // 
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Image = global::RMS.Properties.Resources._1498666014_basics_19;
+            this.btnSearch.Image = global::RMS.Properties.Resources.search25;
             this.btnSearch.Location = new System.Drawing.Point(279, 7);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(35, 31);
--- RMS/RMS/Forms/frmSubLocation.Designer.cs
+++ RMS/RMS/Forms/frmSubLocation.Designer.cs
@@ -154,7 +154,7 @@
             // btnSearch
             // 
             this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSearch.Image = global::RMS.Properties.Resources._1498666014_basics_19;
+            this.btnSearch.Image = global::RMS.Properties.Resources.search25;
             this.btnSearch.Location = new System.Drawing.Point(287, 52);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(35, 31);
