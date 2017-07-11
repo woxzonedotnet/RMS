b4e22234bfd63803fe9c476aa9f045d9e894833b
dilshan8kt <dilshan.kt@gmail.com>
2017-07-11 10:26:26 +0530

    Edit Customer Details Form

--- RMS/RMS/Forms/frmCustomerDetails.cs
+++ RMS/RMS/Forms/frmCustomerDetails.cs
@@ -12,9 +12,33 @@ namespace RMS.Forms
 {
     public partial class frmCustomerDetails : Form
     {
+        #region Variables
+        Point lastClick;
+        #endregion
+
         public frmCustomerDetails()
         {
             InitializeComponent();
+            this.lblTitle.Text = this.Text;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastClick.X;
+                this.Top += e.Y - lastClick.Y;
+            }
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastClick = e.Location;
         }
     }
 }
