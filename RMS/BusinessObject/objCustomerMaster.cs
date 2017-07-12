499c91867f63c83b631d4ae99b025802f2925867
dilshan8kt <dilshan.kt@gmail.com>
2017-07-12 09:04:50 +0530

    Add CustomerMaster Object

---
+++ RMS/BusinessObject/objCustomerMaster.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objCustomerMaster
+    {
+        public string LocationCode { get; set; }
+        public string CustomerCode { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public double CreditLimit { get; set; }
+        public double Discount { get; set; }
+        public string VatNo { get; set; }
+        public bool IsServiceCharge { get; set; }
+        public bool IsCreditCustomer { get; set; }
+        public bool IsComplementary { get; set; }
+    }
+}
