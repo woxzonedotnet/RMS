01b93795a775501b7e7c46d8ab878efee52e8352
dilshan8kt <dilshan.kt@gmail.com>
2017-07-12 15:39:08 +0530

    Add Supplier Master Object

---
+++ RMS/BusinessObject/objSupplierMaster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objSupplierMaster
+    {
+        public string LocationCode { get; set; }
+        public string SupplierCode { get; set; }
+        public string SupplierName { get; set; }
+        public string ContactPerson { get; set; }
+        public string Address { get; set; }
+        public string TellephoneNo { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string SupplierCategoryCode { get; set; }
+        public double Discount { get; set; }
+        public bool Consignment { get; set; }
+        public DateTime EnterdDate { get; set; }
+        public DateTime CancelDate { get; set; }
+        public int Status { get; set; }
+        public bool RelesePendingPO { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
