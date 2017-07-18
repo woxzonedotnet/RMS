d48de613e300b662f5c625e7e209f756219f86a6
dilshan8kt <dilshan.kt@gmail.com>
2017-07-18 15:06:04 +0530

    Add Menu Department Object

---
+++ RMS/BusinessObject/objMenuDepartment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objMenuDepartment
+    {
+        public string LocationCode { get; set; }
+        public string MenuDepartmentCode { get; set; }
+        public string MenuDepartmentName { get; set; }
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
