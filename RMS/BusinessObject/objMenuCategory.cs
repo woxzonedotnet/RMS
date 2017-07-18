d6091f4d35989c7344596c78cd0b39a243287980
dilshan8kt <dilshan.kt@gmail.com>
2017-07-18 17:21:19 +0530

    Add Menu Category Object

---
+++ RMS/BusinessObject/objMenuCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objMenuCategory
+    {
+        public string LocationCode { get; set; }
+        public string MenuDepartmentCode { get; set; }
+        public string MenuCategoryCode { get; set; }
+        public string MenuCategoryName { get; set; }
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
