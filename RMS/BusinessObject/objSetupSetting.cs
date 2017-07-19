784582c4eb60835ece0860106c390c17d176e5fb
dilshan8kt <dilshan.kt@gmail.com>
2017-07-19 12:25:34 +0530

    Add Setup Setting Object

---
+++ RMS/BusinessObject/objSetupSetting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objSetupSetting
+    {
+        public class objSetupSetting
+        {
+            public string LocationCode { get; set; }
+            public decimal ServiceCharge { get; set; }
+            public decimal VAT { get; set; }
+            public decimal GuidCharge { get; set; }
+            public bool IsExists { get; set; }
+        }
+    }
+}
