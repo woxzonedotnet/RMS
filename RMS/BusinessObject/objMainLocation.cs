0ba0fcaa9d5010b51ca1494b2e0b45c674818bc0
dilshan8kt <dilshan.kt@gmail.com>
2017-07-19 12:26:22 +0530

    Add Main Location Object

---
+++ RMS/BusinessObject/objMainLocation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objMainLocation
+    {
+        public string MainLocationCode { get; set; }
+        public string MainLocationName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string TelephoneNo { get; set; }
+        public string FaxNo { get; set; }
+        public string Email { get; set; }
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
