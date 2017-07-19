89b81c1800cfcf447d6ce69b105e6e3c6f6fb939
dilshan8kt <dilshan.kt@gmail.com>
2017-07-19 12:30:43 +0530

    Edit SetupSetting Objects

--- RMS/BusinessObject/objSetupSetting.cs
+++ RMS/BusinessObject/objSetupSetting.cs
@@ -8,13 +8,10 @@ namespace BusinessObject
 {
     public class objSetupSetting
     {
-        public class objSetupSetting
-        {
-            public string LocationCode { get; set; }
-            public decimal ServiceCharge { get; set; }
-            public decimal VAT { get; set; }
-            public decimal GuidCharge { get; set; }
-            public bool IsExists { get; set; }
-        }
+        public string LocationCode { get; set; }
+        public decimal ServiceCharge { get; set; }
+        public decimal VAT { get; set; }
+        public decimal GuidCharge { get; set; }
+        public bool IsExists { get; set; }
     }
 }
