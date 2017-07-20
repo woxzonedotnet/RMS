97c2cb451934caba202149180299267917346fff
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-20 17:26:23 +0530

    Class Capacity Type Design

---
+++ RMS/BusinessLogic/clsCapacityType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject;
+using DataAccess;
+using System.Data;
+
+namespace BusinessLogic
+{
+    public class clsCapacityType
+    {
+        #region Object
+        objCapacityType oCapacityType = new objCapacityType();
+        clsDBConnection cDBConnection = new clsDBConnection();
+        #endregion
+
+        #region GetDepartment Data
+        public DataTable GetCapacityData()
+        {
+            DataTable dtCapacity = cDBConnection.SearchData("tbl_Capacity");
+            return dtCapacity;
+        }
+        #endregion
+    }
+}
