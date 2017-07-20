9709e6c1ee0b1514a44a003f78ec4149dedb4134
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-20 17:27:24 +0530

    Capacity Type object Create

---
+++ RMS/BusinessObject/objCapacityType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objCapacityType
+    {
+        public string CapacityCode { get; set; }
+        public string CapacityName { get; set; }
+        public string CapacitySymbol { get; set; }
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
