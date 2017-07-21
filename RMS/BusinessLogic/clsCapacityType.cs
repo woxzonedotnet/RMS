fe94c9dc36bab57091e1e2414fe9cbc572ffce0e
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-21 13:10:35 +0530

    Class Capacity Type Edit

--- RMS/BusinessLogic/clsCapacityType.cs
+++ RMS/BusinessLogic/clsCapacityType.cs
@@ -16,11 +16,33 @@ namespace BusinessLogic
         clsDBConnection cDBConnection = new clsDBConnection();
         #endregion
 
-        #region GetDepartment Data
+        #region GetCapacity Data
         public DataTable GetCapacityData()
         {
             DataTable dtCapacity = cDBConnection.SearchData("tbl_Capacity");
             return dtCapacity;
+        }
+        #endregion
+
+        #region GetCapacity Data By Code
+        public string GetCapacityDataByCode(string strCapacityCode)
+        {
+            string strWhere = "fldCapacityCode='" + strCapacityCode + "'";
+            DataTable dtCapacity = cDBConnection.SearchData("tbl_Capacity",strWhere);
+            if (dtCapacity.Rows.Count > 0)
+            {
+                oCapacityType.CapacityCode = dtCapacity.Rows[0][0].ToString();
+                oCapacityType.CapacityName = dtCapacity.Rows[0][1].ToString();
+                oCapacityType.CapacitySymbol = dtCapacity.Rows[0][2].ToString();
+
+                //oMenuCategory.IsExists = true;
+            }
+            else
+            {
+                //oMenuCategory.IsExists = false;
+            }
+
+            return oCapacityType.CapacityName;
         }
         #endregion
     }
