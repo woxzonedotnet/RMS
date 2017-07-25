b3bcc655fe90ca5ff5e0219c980717ccd5291c88
Woxzone-Thameesha <popularitinfo@gmail.com>
2017-07-25 15:23:09 +0530

    Purchase Order Class and object

---
+++ RMS/BusinessObject/objPurchaseOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objPurchaseOrder
+    {
+        public string LocationCode { get; set; }
+        public string PurchaseOrderCode { get; set; }
+        public string SubLocationCode { get; set; }
+        public string SupplierCode { get; set; }
+        public DateTime Date { get; set; }
+        public double Purchase { get; set; }
+        public double VAT { get; set; }
+        public double NetAmount { get; set; }
+        public string Remarks { get; set; }
+
+
+        public string ItemCode { get; set; }
+        public string ItemDescription { get; set; }
+        public double UnitPrice { get; set; }
+        public double Quantity { get; set; }
+        public double TaxAmount { get; set; }
+        public double TotalAmount { get; set; }
+        public DataTable dtItemList { get; set; }
+
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
