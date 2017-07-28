23f48c55941cfacf91787720711093ff60945e24
dilshan8kt <dilshan.kt@gmail.com>
2017-07-28 17:19:05 +0530

    Edit Recipe Details

--- RMS/BusinessLogic/clsRecipeDetails.cs
+++ RMS/BusinessLogic/clsRecipeDetails.cs
@@ -13,15 +13,39 @@ namespace BusinessLogic
     {
         #region Objects
         clsDBConnection cDBConnection = new clsDBConnection();
+        objRecipeDetails oRecipeDetails = new objRecipeDetails();
         #endregion
 
 
-        
+        #region Get Purchase Order Data using ItemCode
+        public objRecipeDetails GetPurchaseOrderData(string strLocationCode, string strSubLocationCode, string strRecipeCode)
+        {
+            string strWhere1 = "fldLocationCode='" + strLocationCode + "' fldSubLocationCode='" + strSubLocationCode + "' AND fldRecipeCode='" + strRecipeCode + "'";
+            string strWhere2 = "fldPOCode='" + strRecipeCode + "'";
 
-       
-       
+            DataTable dtPODetails = cDBConnection.SearchData("tbl_RecipeHead", strWhere1);
+            DataTable dtPOItemList = cDBConnection.SearchData("tbl_RecipeDetails", strWhere2);
 
+            if (dtPODetails.Rows.Count > 0)
+            {
+                oRecipeDetails.LocationCode = dtPODetails.Rows[0]["fldLocationCode"].ToString();
+                oRecipeDetails.SubLocationCode = dtPODetails.Rows[0]["fldSubLocationCode"].ToString();
+                oRecipeDetails.RecipeCode = dtPODetails.Rows[0]["fldRecipeCode"].ToString();
+                oRecipeDetails.RecipeName = dtPODetails.Rows[0]["fldRecipeName"].ToString();
+                oRecipeDetails.OrderTypeCode = dtPODetails.Rows[0]["fldOrderTypeCode"].ToString();
+                oRecipeDetails.RecipeCost = Convert.ToDouble(dtPODetails.Rows[0]["fldRecipeCost"]);
 
+                oRecipeDetails.dtRecipeDetails = dtPOItemList;
+
+                oRecipeDetails.IsExists = true;
+            }
+            else
+            {
+                oRecipeDetails.IsExists = false;
+            }
+            return oRecipeDetails;
+        }
+        #endregion
 
     }
 }
