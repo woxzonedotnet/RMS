a57e8816ded8199cce70af6e119ce00f71bf389c
dilshan8kt <dilshan.kt@gmail.com>
2017-07-28 13:48:52 +0530

    Add Order Type Class

---
+++ RMS/BusinessLogic/clsOrderType.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using BusinessObject;
+using System.Data;
+
+namespace BusinessLogic
+{
+    public class clsOrderType
+    {
+        #region Objects
+        clsDBConnection cDBConnection = new clsDBConnection();
+        objOrderType oOrderType = new objOrderType();
+        #endregion
+
+        #region GetRecipe Data Using Recipe Code
+        public DataTable GetDepartmentData(string strLocationCode)
+        {
+            string strWhere = "fldLocationCode='" + strLocationCode + "'";
+
+            DataTable dtOrderType = cDBConnection.SearchData("tbl_OrderType", strWhere);
+
+            if (dtOrderType.Rows.Count > 0)
+            {
+                oOrderType.LocationCode = dtOrderType.Rows[0]["fldLocationCode"].ToString();
+                oOrderType.OrderTypeCode = dtOrderType.Rows[0]["fldOrderTypeCode"].ToString();
+                oOrderType.OrderTypeName = dtOrderType.Rows[0]["fldOrderTypeName"].ToString();
+                oOrderType.Status = Convert.ToInt16(dtOrderType.Rows[0]["fldStatus"]);
+
+                oOrderType.IsExists = true;
+            }
+            else
+            {
+                oOrderType.IsExists = false;
+            }
+            return dtOrderType;
+        }
+        #endregion
+
+    }
+}
