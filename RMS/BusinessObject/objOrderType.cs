440fb449f426920d501264c694784bb7aa869692
dilshan8kt <dilshan.kt@gmail.com>
2017-07-28 13:49:55 +0530

    Add Order Type Object

---
+++ RMS/BusinessObject/objOrderType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objOrderType
+    {
+        public string LocationCode { get; set; }
+        public string OrderTypeCode { get; set; }
+        public string OrderTypeName { get; set; }
+        public int Status { get; set; }
+        public bool IsExists { get; set; }
+    }
+}
