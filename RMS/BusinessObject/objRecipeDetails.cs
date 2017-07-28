681b06b5d8e6275c3caee91137613f84fd07a158
dilshan8kt <dilshan.kt@gmail.com>
2017-07-28 17:19:26 +0530

    Edit Recipe Details Object

--- RMS/BusinessObject/objRecipeDetails.cs
+++ RMS/BusinessObject/objRecipeDetails.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,13 @@ namespace BusinessObject
 {
     public class objRecipeDetails
     {
-        public string RecipeCode { get; set;}
-        public string OrderType { get; set; }
-
-        public string Location { get; set; }
-
-        public string FullDescription { get; set; }
-
-        public string RecipeCost { get; set; }
-
+        public string LocationCode { get; set; }
+        public string SubLocationCode { get; set; }
+        public string RecipeCode { get; set; }
+        public string RecipeName { get; set; }
+        public string OrderTypeCode { get; set; }
+        public double RecipeCost { get; set; }
+        public DataTable dtRecipeDetails { get; set; }
         public bool IsExists { get; set; }
     }
 }
