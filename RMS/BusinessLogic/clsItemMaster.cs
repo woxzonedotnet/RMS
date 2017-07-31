44364f3d9736a4fba73f8fe16789d87ee5b36033
dilshan8kt <dilshan.kt@gmail.com>
2017-07-31 09:53:06 +0530

    Edit Item Master Class

--- RMS/BusinessLogic/clsItemMaster.cs
+++ RMS/BusinessLogic/clsItemMaster.cs
@@ -44,30 +44,30 @@ namespace BusinessLogic
             arrParameter[8, 1] = oItemMaster.Supplier;
             arrParameter[9, 0] = "@mfldMenuCategoryCode";
             arrParameter[9, 1] = oItemMaster.MenuCategory;
-            arrParameter[10, 0] = "@mfldUnit";
-            arrParameter[10, 1] = oItemMaster.Unit;
-            arrParameter[11, 0] = "@mfldMinimumGP";
-            arrParameter[11, 1] = oItemMaster.MinimumGP;
-            arrParameter[12, 0] = "@mfldPackageSize";
-            arrParameter[12, 1] = oItemMaster.PackageSize;
-            arrParameter[13, 0] = "@mfldCapacity";
-            arrParameter[13, 1] = oItemMaster.Capacity;
-            arrParameter[14, 0] = "@mfldCapacityCode";
-            arrParameter[14, 1] = oItemMaster.CapacityType;
-            arrParameter[15, 0] = "@mfldCostPrice";
-            arrParameter[15, 1] = oItemMaster.CostPrice;
-            arrParameter[16, 0] = "@mfldWholeSalePrice";
-            arrParameter[16, 1] = oItemMaster.WholeSalePrice;
-            arrParameter[17, 0] = "@mfldSellingPrice";
-            arrParameter[17, 1] = oItemMaster.SellingPrice;
-            arrParameter[18, 0] = "@mfldReOrderLevel";
-            arrParameter[18, 1] = oItemMaster.ReOrderLevel;
-            arrParameter[19, 0] = "@mfldReOrderQty";
-            arrParameter[19, 1] = oItemMaster.ReOrderQty;
-            arrParameter[20, 0] = "@mfldReOrderMax";
-            arrParameter[20, 1] = oItemMaster.ReOrderMax;
-            arrParameter[21, 0] = "@mfldStatus";
-            arrParameter[21, 1] = oItemMaster.Status;
+            //arrParameter[10, 0] = "@mfldUnit";
+            //arrParameter[10, 1] = oItemMaster.Unit;
+            arrParameter[10, 0] = "@mfldMinimumGP";
+            arrParameter[10, 1] = oItemMaster.MinimumGP;
+            arrParameter[11, 0] = "@mfldPackageSize";
+            arrParameter[11, 1] = oItemMaster.PackageSize;
+            arrParameter[12, 0] = "@mfldCapacity";
+            arrParameter[12, 1] = oItemMaster.Capacity;
+            arrParameter[13, 0] = "@mfldCapacityCode";
+            arrParameter[13, 1] = oItemMaster.CapacityType;
+            arrParameter[14, 0] = "@mfldCostPrice";
+            arrParameter[14, 1] = oItemMaster.CostPrice;
+            arrParameter[15, 0] = "@mfldWholeSalePrice";
+            arrParameter[15, 1] = oItemMaster.WholeSalePrice;
+            arrParameter[16, 0] = "@mfldSellingPrice";
+            arrParameter[16, 1] = oItemMaster.SellingPrice;
+            arrParameter[17, 0] = "@mfldReOrderLevel";
+            arrParameter[17, 1] = oItemMaster.ReOrderLevel;
+            arrParameter[18, 0] = "@mfldReOrderQty";
+            arrParameter[18, 1] = oItemMaster.ReOrderQty;
+            arrParameter[19, 0] = "@mfldReOrderMax";
+            arrParameter[19, 1] = oItemMaster.ReOrderMax;
+            arrParameter[20, 0] = "@mfldStatus";
+            arrParameter[20, 1] = oItemMaster.Status;
 
             return cDBConnection.Insert("sp_insert_update_itemMaster", arrParameter);
         }
@@ -90,60 +90,9 @@ namespace BusinessLogic
         #endregion
 
         #region Get ItemData using ItemCode
-        public objItemMaster GetItemData(string strItemCode)
+        public objItemMaster GetItemData(string strLocationCode, string strItemCode)
         {
-            string strWhere = "fldItemCode='" + strItemCode + "'";
-
-            DataTable dtItemMaster = cDBConnection.SearchData("tbl_ItemMaster",strWhere);
-            DataTable dtItemPrice = cDBConnection.SearchData("tbl_ItemPrice", strWhere);
-            DataTable dtItemReOrder = cDBConnection.SearchData("tbl_ItemReOrder", strWhere);
-
-            if (dtItemMaster.Rows.Count > 0)
-            {
-                oItemMaster.LocationCode = dtItemMaster.Rows[0][0].ToString();
-                oItemMaster.ItemCode = dtItemMaster.Rows[0][1].ToString();
-                oItemMaster.BarCode = dtItemMaster.Rows[0][2].ToString();
-                oItemMaster.Description = dtItemMaster.Rows[0][3].ToString();
-                oItemMaster.Consignm = Convert.ToBoolean(dtItemMaster.Rows[0][4].ToString());
-                oItemMaster.Weighted = Convert.ToBoolean(dtItemMaster.Rows[0][5].ToString());
-                oItemMaster.Department = dtItemMaster.Rows[0][6].ToString();
-                oItemMaster.Category = dtItemMaster.Rows[0][7].ToString();
-                oItemMaster.Supplier = dtItemMaster.Rows[0][8].ToString();
-                oItemMaster.MenuCategory = dtItemMaster.Rows[0][9].ToString();
-                oItemMaster.Unit = Convert.ToInt32(dtItemMaster.Rows[0][10].ToString());
-                oItemMaster.MinimumGP = Convert.ToDouble(dtItemMaster.Rows[0][11].ToString());
-                oItemMaster.PackageSize = Convert.ToDouble(dtItemMaster.Rows[0][12].ToString());
-                oItemMaster.Capacity = Convert.ToDouble(dtItemMaster.Rows[0][13].ToString());
-                oItemMaster.CapacityType = dtItemMaster.Rows[0][14].ToString();
-                oItemMaster.Status = Convert.ToInt16(dtItemMaster.Rows[0][15]);
-
-                //Price Table
-                oItemMaster.CostPrice = Convert.ToDouble(dtItemPrice.Rows[0][1].ToString());
-                oItemMaster.WholeSalePrice = Convert.ToDouble(dtItemPrice.Rows[0][2].ToString());
-                oItemMaster.SellingPrice = Convert.ToDouble(dtItemPrice.Rows[0][3].ToString());
-
-                //ReOrder Table
-                oItemMaster.ReOrderLevel = Convert.ToDouble(dtItemReOrder.Rows[0][1].ToString());
-                oItemMaster.ReOrderQty = Convert.ToDouble(dtItemReOrder.Rows[0][2].ToString());
-                oItemMaster.ReOrderMax = Convert.ToDouble(dtItemReOrder.Rows[0][3].ToString());
-
-
-                oItemMaster.IsExists = true;
-            }
-            else
-            {
-                oItemMaster.IsExists = false;
-            }
-
-
-            return oItemMaster;
-        }
-        #endregion
-
-        #region Get ItemData using ItemCode
-        public objItemMaster GetItemData(string strLocationCode,string strItemCode)
-        {
-            string strWhere1 = "fldItemCode='" + strItemCode + "' AND fldLocationCode='"+strLocationCode+"'";
+            string strWhere1 = "fldItemCode='" + strItemCode + "' AND fldLocationCode='" + strLocationCode + "'";
             string strWhere2 = "fldItemCode='" + strItemCode + "'";
 
             DataTable dtItemMaster = cDBConnection.SearchData("tbl_ItemMaster", strWhere1);
@@ -152,33 +101,32 @@ namespace BusinessLogic
 
             if (dtItemMaster.Rows.Count > 0)
             {
-                oItemMaster.LocationCode = dtItemMaster.Rows[0][0].ToString();
-                oItemMaster.ItemCode = dtItemMaster.Rows[0][1].ToString();
-                oItemMaster.BarCode = dtItemMaster.Rows[0][2].ToString();
-                oItemMaster.Description = dtItemMaster.Rows[0][3].ToString();
-                oItemMaster.Consignm = Convert.ToBoolean(dtItemMaster.Rows[0][4].ToString());
-                oItemMaster.Weighted = Convert.ToBoolean(dtItemMaster.Rows[0][5].ToString());
-                oItemMaster.Department = dtItemMaster.Rows[0][6].ToString();
-                oItemMaster.Category = dtItemMaster.Rows[0][7].ToString();
-                oItemMaster.Supplier = dtItemMaster.Rows[0][8].ToString();
-                oItemMaster.MenuCategory = dtItemMaster.Rows[0][9].ToString();
-                oItemMaster.Unit = Convert.ToInt32(dtItemMaster.Rows[0][10].ToString());
-                oItemMaster.MinimumGP = Convert.ToDouble(dtItemMaster.Rows[0][11].ToString());
-                oItemMaster.PackageSize = Convert.ToDouble(dtItemMaster.Rows[0][12].ToString());
-                oItemMaster.Capacity = Convert.ToDouble(dtItemMaster.Rows[0][13].ToString());
-                oItemMaster.CapacityType = dtItemMaster.Rows[0][14].ToString();
-                oItemMaster.Status = Convert.ToInt16(dtItemMaster.Rows[0][15]);
+                oItemMaster.LocationCode = dtItemMaster.Rows[0]["fldLocationCode"].ToString();
+                oItemMaster.ItemCode = dtItemMaster.Rows[0]["fldItemCode"].ToString();
+                oItemMaster.BarCode = dtItemMaster.Rows[0]["fldBarcode"].ToString();
+                oItemMaster.Description = dtItemMaster.Rows[0]["fldDescription"].ToString();
+                oItemMaster.Consignm = Convert.ToBoolean(dtItemMaster.Rows[0]["fldConsignm"].ToString());
+                oItemMaster.Weighted = Convert.ToBoolean(dtItemMaster.Rows[0]["fldWeighted"].ToString());
+                oItemMaster.Department = dtItemMaster.Rows[0]["fldDepartmentCode"].ToString();
+                oItemMaster.Category = dtItemMaster.Rows[0]["fldCategoryCode"].ToString();
+                oItemMaster.Supplier = dtItemMaster.Rows[0]["fldSupplierCode"].ToString();
+                oItemMaster.MenuCategory = dtItemMaster.Rows[0]["fldMenuCategoryCode"].ToString();
+                //oItemMaster.Unit = Convert.ToInt32(dtItemMaster.Rows[0]["fldUnit"].ToString());
+                oItemMaster.MinimumGP = Convert.ToDouble(dtItemMaster.Rows[0]["fldMinimumGP"].ToString());
+                oItemMaster.PackageSize = Convert.ToDouble(dtItemMaster.Rows[0]["fldPackageSize"].ToString());
+                oItemMaster.Capacity = Convert.ToDouble(dtItemMaster.Rows[0]["fldCapacity"].ToString());
+                oItemMaster.CapacityType = dtItemMaster.Rows[0]["fldCapacityCode"].ToString();
+                oItemMaster.Status = Convert.ToInt16(dtItemMaster.Rows[0]["fldStatus"]);
 
                 //Price Table
-                oItemMaster.CostPrice = Convert.ToDouble(dtItemPrice.Rows[0][1].ToString());
-                oItemMaster.WholeSalePrice = Convert.ToDouble(dtItemPrice.Rows[0][2].ToString());
-                oItemMaster.SellingPrice = Convert.ToDouble(dtItemPrice.Rows[0][3].ToString());
+                oItemMaster.CostPrice = Convert.ToDouble(dtItemPrice.Rows[0]["fldCostPrice"].ToString());
+                oItemMaster.WholeSalePrice = Convert.ToDouble(dtItemPrice.Rows[0]["fldWholeSalePrice"].ToString());
+                oItemMaster.SellingPrice = Convert.ToDouble(dtItemPrice.Rows[0]["fldSellingPrice"].ToString());
 
                 //ReOrder Table
-                oItemMaster.ReOrderLevel = Convert.ToDouble(dtItemReOrder.Rows[0][1].ToString());
-                oItemMaster.ReOrderQty = Convert.ToDouble(dtItemReOrder.Rows[0][2].ToString());
-                oItemMaster.ReOrderMax = Convert.ToDouble(dtItemReOrder.Rows[0][3].ToString());
-
+                oItemMaster.ReOrderLevel = Convert.ToDouble(dtItemReOrder.Rows[0]["fldReOrderLevel"].ToString());
+                oItemMaster.ReOrderQty = Convert.ToDouble(dtItemReOrder.Rows[0]["fldReOrderQty"].ToString());
+                oItemMaster.ReOrderMax = Convert.ToDouble(dtItemReOrder.Rows[0]["fldReOrderMax"].ToString());
 
                 oItemMaster.IsExists = true;
             }
@@ -186,8 +134,6 @@ namespace BusinessLogic
             {
                 oItemMaster.IsExists = false;
             }
-
-
             return oItemMaster;
         }
         #endregion
