93726af68fc38bbbfe063754a8b1d6f5570b67a4
dilshan8kt <dilshan.kt@gmail.com>
2017-08-01 11:36:40 +0530

    Add Report Master Objects

---
+++ RMS/BusinessObject/objReportMaster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject
+{
+    public class objReportMaster
+    {
+        public string LocationCode{get;set;}
+        public string ReportID { get; set; }
+        public string ReportName { get; set; }
+        public string ReportTitle { get; set; }
+        public string SelectionFormular { get; set; }
+        public string SelectedTable { get; set; }
+    }
+}
