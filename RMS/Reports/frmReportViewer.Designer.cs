53547ee2655c615099bfb4e5926657df0c551ba3
dilshan8kt <dilshan.kt@gmail.com>
2017-08-01 15:14:02 +0530

    Add Report Layer

---
+++ RMS/Reports/frmReportViewer.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Reports
+{
+    partial class frmReportViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.crystalReportViewer = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.panel3.Controls.Add(this.btnMinimize);
+            this.panel3.Controls.Add(this.lblTitle);
+            this.panel3.Controls.Add(this.btnExit);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(800, 31);
+            this.panel3.TabIndex = 69;
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
+            this.panel3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseMove);
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(150)))), ((int)(((byte)(153)))));
+            this.btnMinimize.Location = new System.Drawing.Point(726, 0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(37, 31);
+            this.btnMinimize.TabIndex = 5;
+            this.btnMinimize.Text = "_";
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTitle.Location = new System.Drawing.Point(13, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(93, 16);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "Report Viewer";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(41)))), ((int)(((byte)(46)))));
+            this.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Maroon;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(146)))), ((int)(((byte)(150)))), ((int)(((byte)(153)))));
+            this.btnExit.Location = new System.Drawing.Point(763, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(37, 31);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "X";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // crystalReportViewer
+            // 
+            this.crystalReportViewer.ActiveViewIndex = -1;
+            this.crystalReportViewer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer.Location = new System.Drawing.Point(0, 31);
+            this.crystalReportViewer.Name = "crystalReportViewer";
+            this.crystalReportViewer.Size = new System.Drawing.Size(800, 431);
+            this.crystalReportViewer.TabIndex = 70;
+            this.crystalReportViewer.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // frmReportViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 462);
+            this.Controls.Add(this.crystalReportViewer);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmReportViewer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Report Viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmReportViewer_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnExit;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer;
+        private System.Windows.Forms.Button btnMinimize;
+
+    }
+}
---
+++ RMS/Reports/frmReportViewer.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BusinessLogic;
+using BusinessObject;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
+namespace Reports
+{
+    public partial class frmReportViewer : Form
+    {
+        #region Objects
+        objReportMaster oReportMaster = new objReportMaster();
+        clsReportMaster cReportMaster = new clsReportMaster();
+        ReportDocument cReportDocument = new ReportDocument();
+        ParameterField cParameterField;
+        ParameterDiscreteValue cParameterDiscreteValue;
+        ParameterFields cParameterFields;
+        #endregion
+
+        #region Variables
+        Point lastClick;
+        System.Object[,] arrParamFields;
+
+        private string strSelectionFormular = string.Empty;
+        private string strLocationCode;
+        private int iReportID;
+        #endregion
+
+        #region Constructor
+        public frmReportViewer()
+        {
+            InitializeComponent();
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        public frmReportViewer(int ReportID, string LocationCode)
+        {
+            InitializeComponent();
+            iReportID = ReportID;
+            strLocationCode = LocationCode;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        public frmReportViewer(int ReportID, string LocationCode, string SelectionFormular)
+        {
+            InitializeComponent();
+            iReportID = ReportID;
+            strSelectionFormular = SelectionFormular;
+            strLocationCode = LocationCode;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+
+        public frmReportViewer(int ReportID, string LocationCode, string SelectionFormular, System.Object[,] arrParameter)
+        {
+            InitializeComponent();
+
+            iReportID = ReportID;
+            strSelectionFormular = SelectionFormular;
+            strLocationCode = LocationCode;
+
+            arrParamFields = new object[arrParameter.GetLength(0) - 1, 2];
+            arrParamFields = arrParameter;
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+        }
+        #endregion
+
+        #region Pannel Move
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastClick = e.Location;
+        }
+
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastClick.X;
+                this.Top += e.Y - lastClick.Y;
+            }
+        }
+        #endregion
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void LoadReportDeta()
+        {
+            oReportMaster = cReportMaster.GetReports(iReportID);
+        }
+
+        private void frmReportViewer_Load(object sender, EventArgs e)
+        {
+            LoadReportDeta();
+            this.MaximizedBounds = Screen.FromHandle(this.Handle).WorkingArea;
+            //cReportDocument.FileName = Application.StartupPath + "\\Reports\\" + oReportMaster.ReportName;
+            cReportDocument.FileName = @"E:\WOXZONE RMS\RMS2\RMS\Reports\" + oReportMaster.ReportName;
+            cReportDocument.RecordSelectionFormula = strSelectionFormular;
+            crystalReportViewer.ReportSource = cReportDocument;
+        }
+
+
+        private void PassParemeterFields(System.Object[,] arrParameter)
+        {
+            for (int i = 0; i <= arrParameter.GetLength(0) - 1; i++)
+            {
+                cParameterField = new ParameterField();
+                cParameterField.Name = arrParameter[i, 0].ToString();
+
+                cParameterDiscreteValue = new ParameterDiscreteValue();
+                cParameterDiscreteValue.Value = arrParameter[i, 1].ToString();
+                cParameterField.CurrentValues.Add(cParameterDiscreteValue);
+
+                cParameterFields.Add(cParameterField);
+                crystalReportViewer.ParameterFieldInfo = cParameterFields;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+
+    }
+}
