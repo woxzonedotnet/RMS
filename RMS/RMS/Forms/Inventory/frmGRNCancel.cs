49d7453ce9fb4607bfd34d5a7137f04249e4d25a
dilshan8kt <dilshan.kt@gmail.com>
2017-08-09 16:42:53 +0530

    Main Structure

--- RMS/RMS/Forms/Inventory/frmGRNCancel.cs
+++ RMS/RMS/Forms/Inventory/frmGRNCancel.cs
@@ -7,14 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
+using BusinessObject;
 
 namespace RMS.Forms.Inventory
 {
     public partial class frmGRNCancel : Form
     {
+        #region Objects
+        clsCommonMethods cCommonMethods = new clsCommonMethods();
+        clsGlobleVariable cGlobleVariable = new clsGlobleVariable();
+        clsSubLocation cSubLocation = new clsSubLocation();
+        clsSupplierMaster cSupplierMaster = new clsSupplierMaster();
+        clsDocumentNumber cDocumentNumber = new clsDocumentNumber();
+        objDocumentNumber oDocumentNumber = new objDocumentNumber();
+        #endregion
+
+        #region Variables
+        Point lastClick;
+        string DocumentCode = "CGRN";
+        #endregion
+
         public frmGRNCancel()
         {
             InitializeComponent();
+            cCommonMethods.loadComboRMS(cSubLocation.GetSubLocationData(cGlobleVariable.LocationCode), cmbLocation, 2);
+            cCommonMethods.loadComboRMS(cSupplierMaster.GetSupplierData(cGlobleVariable.LocationCode), cmbSupplier, 2);
+            LoadDocumentNumber();
+            clear();
+        }
+
+        public void LoadDocumentNumber()
+        {
+            this.txtGRNCancelNo.Text = cDocumentNumber.LoadDocNumber(cGlobleVariable.UniqID, DocumentCode, cGlobleVariable.LocationCode, cCommonMethods.DateYear());
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        #region Form Move
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastClick.X;
+                this.Top += e.Y - lastClick.Y;
+            }
+        }
+
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastClick = e.Location;
+        }
+        #endregion
+
+        #region Clear
+        private void clear() 
+        {
+            cCommonMethods.ClearForm(this);
+            this.txtGrossTotal.Text = "0.00";
+            this.txtVatAmount.Text = "0.00";
+            this.txtDiscount.Text = "0.00";
+            this.txtNetAmount.Text = "0.00";
+            this.btnPrint.Enabled = false;
+        }
+        #endregion
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
