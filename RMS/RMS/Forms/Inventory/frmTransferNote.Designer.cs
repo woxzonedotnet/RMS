2ae34d4d27d1fa99c1d684096912c6f445b13461
dilshan8kt <dilshan.kt@gmail.com>
2017-08-24 15:14:43 +0530

    TransferNote

--- RMS/RMS/Forms/Inventory/frmTransferNote.Designer.cs
+++ RMS/RMS/Forms/Inventory/frmTransferNote.Designer.cs
@@ -124,6 +124,7 @@
             this.btnPrint.TabIndex = 92;
             this.btnPrint.Text = "Print";
             this.btnPrint.UseVisualStyleBackColor = false;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
             // 
             // txtTotal
             // 
